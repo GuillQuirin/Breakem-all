bf24400279180fe07f29f4ce049f9736b5746fb4
dydyFos <YOUR EMAIL ADRESS>
2016-02-07 23:18:59 +0100

    page noir et blanche

--- vue/template/header.php
+++ vue/template/header.php
@@ -3,9 +3,13 @@
 
 	<head>
 		<meta charset="UTF-8">
+		<meta name="robots" content="index,follow" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<title>Break'em all</title>
 		<meta name="description" content="Tournois de jeux vidéo">
+		<link rel="stylesheet" href="public/css/style-general.css">
+		<link rel="stylesheet" href="public/css/style-fiches-jeux.css">
+		<link rel="stylesheet" href="public/css/style-reset.css">
 		<link rel="stylesheet" href="public/css/style.css">
 	</head>
 
--- vue/tournoi.php
+++ vue/tournoi.php
@@ -1,20 +1,303 @@
 <?php
 	require_once('template/header.php');
 ?>
-	<section class="tournoi-choix-jeu relative">
-		<h2>Choisis un jeu</h2>
-		<article class="choix-jeu">
-			<h4>Battlefield 3</h4>
-			<img class="hover-only absolute" src="public/img/bf3-hovered-bw.jpg" alt="bf3">
-		</article>
-		<article class="choix-jeu">
-			<h4>Super Smash Bros Melee</h4>
-			<img class="hover-only absolute" src="public/img/bf3-hovered-bw.jpg" alt="bf3">
-		</article>
-		<article class="choix-jeu">
-			<h4>Fifa 2016</h4>
-			<img class="hover-only absolute" src="public/img/bf3-hovered-bw.jpg" alt="bf3">
-		</article>
+	<section class="ta-center tournoi-choix-jeu">
+		<div class="max-container relative">
+			<h2 class="bande-titre">En ce moment</h2>
+			<div class="changer-choix-container">
+				<ul>
+					<li><a>bf3</a></li>
+					<li class="choix-actif"><a>ssb</a></li>
+					<li><a>lol</a></li>
+					<li><a>fifa</a></li>
+					<li><a>css</a></li>
+					<li><a>nfs</a></li>
+				</ul>
+			</div>
+			<div class="container-choix-jeu">
+				<article class="choix-jeu choix-jeu-bf3">
+					<h4>Battlefield3</h4>				
+					<div class="hover-title-only"><p>Battlefield3</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>6 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois bf3" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement bf3" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter bf3 aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-actif choix-jeu-ssb">
+					<h4>Super Smash Bros Melee</h4>				
+					<div class="hover-title-only"><p>Super Smash Bros</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>3 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois ssb" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement ssb" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter ssb aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-lol">
+					<h4>League Of Legends</h4>	
+					<div class="hover-title-only"><p>League Of Legends</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>8 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois lol" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement lol" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter lol aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+			</div>
+			<div class="container-choix-jeu-arrow-left absolute"></div>
+			<div class="container-choix-jeu-arrow-right absolute"></div>
+		</div>		
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-description">
+		<div class="max-container">
+			<h3>Break'em all - Ceci est la phrase punchline</h3>
+			<p>Break'em all sur les tournois populaires, ou affine ta recherche par mode, console ou date</p>
+		</div>
+	</section>
+	
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-mode relative">
+		<div class="max-container">
+			<h2 class="bande-titre">Où seras tu ?</h2>	
+			<div class="relative choix-offline">
+				<div class="title-container">
+					<h3>offline</h3>
+				</div>
+				<img class="absolute" src="public/img/offline-mode-bg.jpg" alt="mode">
+			</div>
+			<div class="relative choix-online">
+				<div class="title-container">
+					<h3>online</h3>
+				</div>
+				<img class="absolute" src="public/img/online-bw-mode-bg.jpg" alt="mode">
+			</div>
+		</div>		
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-console">		
+		<div class="max-container relative">
+			<h2 class="bande-titre">Ta console</h2>
+			<div class="changer-choix-container">
+				<ul>
+					<li><a>ps</a></li>
+					<li class="choix-actif"><a>pc</a></li>
+					<li><a>wii</a></li>
+					<li><a>n64</a></li>
+					<li><a>xbox</a></li>
+				</ul>
+			</div>
+			<div class="container-choix-jeu">
+				<article class="choix-jeu choix-jeu-bf3">
+					<h4>Battlefield3</h4>				
+					<div class="hover-title-only"><p>Battlefield3</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>6 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois bf3" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement bf3" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter bf3 aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-css">
+					<h4>Counter Strike Source</h4>		
+					<div class="hover-title-only"><p>Counter Strike Source</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>7 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois css" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement css" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter css aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-lol">
+					<h4>League Of Legends</h4>	
+					<div class="hover-title-only"><p>League Of Legends</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>8 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois lol" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement lol" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter lol aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+			</div>
+		</div>
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-date">
+		<div class="max-container">
+			<h2 class="bande-titre">Tournois par date</h2>
+			<div class="tri-calendrier">
+				<div class="tri-jours">
+					<label for="jour">Prochains jours</label>
+					<select name="jour">
+						<option value="j1">j+1</option>
+						<option value="j2">j+2</option>
+						<option value="j3">j+3</option>
+						<option value="j4">j+4</option>
+						<option value="j5">j+5</option>
+						<option value="j6">j+6</option>
+					</select>
+				</div>
+				<div class="tri-semaines">
+					<label for="semaine">Prochaines semaines</label>
+					<select name="semaine">
+						<option value="n1">n+1</option>
+						<option value="n2">n+2</option>
+						<option value="n3">n+3</option>
+					</select>
+				</div>
+				<div class="tri-date-actif tri-mois">
+					<label for="mois">Prochains mois</label>
+					<select name="mois">
+						<option value="m1">m+1</option>
+						<option value="m2">m+2</option>
+						<option value="m3">m+3</option>
+					</select>
+				</div>
+			</div>
+			<div class="prochains-tournois-tries">
+				<table class="table-triee">
+					<thead>
+						<tr>
+							<th>Jeu</th>
+							<th>Orga</th>
+							<th>Console</th>
+							<th>Ouvert</th>
+							<th>Joueurs-max</th>
+							<th>Equipe</th>
+							<th class="tri-actif">Date</th>
+						</tr>
+					</thead>					
+					<tbody>
+						<tr>							
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>ouvert</td>
+							<td>32</td>
+							<td>equipe-8</td>
+							<td class="colonne-triee">15/29/03</td>
+						</tr>
+						<tr>
+							<td>CSS</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>sur demande</td>
+							<td>10</td>
+							<td>equipe-5</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PS3</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>equipe-16</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>LOL</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>invitation</td>
+							<td>10</td>
+							<td>equipe-5-auto</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>SSB</td>
+							<td>pseudo</td>
+							<td>N64</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>individuel</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>FIFA16</td>
+							<td>pseudo</td>
+							<td>XboxOne</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>equipe-2</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>guilde-only</td>
+							<td>48</td>
+							<td>equipe-4</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>LOL</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>ouvert</td>
+							<td>10</td>
+							<td>equipe-5-libre</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>CSS</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>invitation-only</td>
+							<td>16</td>
+							<td>equipe-8-auto</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>guilde-only</td>
+							<td>32</td>
+							<td>equipe-8</td>
+							<td class="colonne-triee">16/25/03</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr>
+							<th>Jeu</th>
+							<th>Orga</th>
+							<th>Console</th>
+							<th>Ouvert</th>
+							<th>Joueurs-max</th>
+							<th>Equipe</th>
+							<th>Date</th>
+						</tr>
+					</tfoot>
+				</table>
+			</div>
+		</div>
 	</section>
 <?php
 	require_once('template/footer.php');
