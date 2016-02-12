cc7c538733a13b1d63bba737a5b06dbe5ddf6197
dydyFos <YOUR EMAIL ADRESS>
2016-02-12 21:33:51 +0100

    supression des vieux dossiers et fichiers avant méthode MVC déjà testée sur branche locale

--- vue/template/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-	<footer>
-	</footer>
-	</body>
-	<script src="public/script/script-fiches-jeux-events.js"></script>
-	<script src="public/script/script-fiches-jeux-functions.js"></script>
-</html>
