691903b266f576368d913ef0294a7b43183d70a1
dydyFos <YOUR EMAIL ADRESS>
2016-02-12 21:35:28 +0100

    Starting MVC developping mode --> a noter: rajouter "conf.inc.php" et  "core/routing.class.php" dans votre fichier '.gitignore' et adaptez les à vos configurations respectives

---
+++ views/tournoiDOM.php
@@ -0,0 +1,389 @@
+<div class="max-container">
+	<div class="fixed tournoi-first-image-background">
+		<img class="absolute" src="web/img/first-bg-web.jpg" alt="epic-fights">
+	</div>
+	<section class="ta-center tournoi-choix-jeu">
+		<div class="max-container relative">
+			<h2 class="titre2 ta-left">Les tendances<span><br>Check les tournois du moment</span></h2>
+			<div class="changer-choix-container">
+				<ul>
+					<li><a>bf3</a></li>
+					<li class="choix-actif"><a>ssb</a></li>
+					<li><a>lol</a></li>
+					<li><a>fifa</a></li>
+					<li><a>css</a></li>
+					<li><a>nfs</a></li>
+				</ul>
+			</div>
+			<div class="mobile-only changer-choix-container-mobile">
+				<div class="menu-opener relative">
+					<div></div>
+					<div></div>
+					<div></div>
+					<ul class="absolute">
+						<li><a>bf3</a></li>
+						<li class="choix-actif"><a>ssb</a></li>
+						<li><a>lol</a></li>
+						<li><a>fifa</a></li>
+						<li><a>css</a></li>
+						<li><a>nfs</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="container-choix-jeu relative">
+				<article class="choix-jeu choix-jeu-bf3">
+					<div class="hover-title-only"><p>Battlefield3</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>6 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois bf3" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement bf3" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter bf3 aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-ssb">	
+					<div class="hover-title-only"><p>Super Smash Bros</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>3 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois ssb" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement ssb" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter ssb aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-lol">	
+					<div class="hover-title-only"><p>League Of Legends</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>8 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois lol" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement lol" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter lol aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+			</div>
+			<div class="container-choix-jeu-arrow-left absolute"></div>
+			<div class="container-choix-jeu-arrow-right absolute"></div>
+		</div>		
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-description">
+		<div class="max-container">
+			<h3>Break'em all - Ceci est la phrase punchline</h3>
+			<p>Break'em all sur les tournois populaires, ou affine ta recherche par mode, console ou date</p>
+		</div>
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-mode relative">
+		<div class="max-container">
+			<h2 class="titre2">Où seras tu ?</h2>	
+			<div class="relative choix-offline">
+				<div class="title-container">
+					<h3>offline</h3>
+				</div>
+				<img class="absolute" src="web/img/offline-mode-bg.jpg" alt="mode">
+			</div>
+			<div class="relative choix-online">
+				<div class="title-container">
+					<h3>online</h3>
+				</div>
+				<img class="absolute" src="web/img/online-bw-mode-bg.jpg" alt="mode">
+			</div>
+		</div>		
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-console">		
+		<div class="max-container relative">
+			<h2 class="titre2">Ta console</h2>
+			<div class="changer-choix-container">
+				<ul>
+					<li><a>ps</a></li>
+					<li class="choix-actif"><a>pc</a></li>
+					<li><a>wii</a></li>
+					<li><a>n64</a></li>
+					<li><a>xbox</a></li>
+				</ul>
+			</div>
+			<div class="mobile-only changer-choix-container-mobile">
+				<div class="menu-opener relative">
+					<div></div>
+					<div></div>
+					<div></div>
+					<ul class="absolute">
+						<li><a>ps</a></li>
+						<li class="choix-actif"><a>pc</a></li>
+						<li><a>wii</a></li>
+						<li><a>n64</a></li>
+						<li><a>xbox</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="container-choix-jeu">				
+				<article class="choix-jeu choix-jeu-css">
+					<h4>Counter Strike Source</h4>		
+					<div class="hover-title-only"><p>Counter Strike Source</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>7 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois css" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement css" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter css aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-actif choix-jeu-lol">
+					<h4>League Of Legends</h4>	
+					<div class="hover-title-only"><p>League Of Legends</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>8 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois lol" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement lol" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter lol aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+				<article class="choix-jeu choix-jeu-bf3">
+					<h4>Battlefield3</h4>				
+					<div class="hover-title-only"><p>Battlefield3</p></div>
+					<div class="btn-container relative">
+						<div class="hover-desc-only absolute"><p>6 tournois à venir</p></div>
+						<button title="rechercher les prochains tournois bf3" class="search-btn absolute"><span class="absolute"></span></button>
+						<button title="classement bf3" class="fight-btn absolute"><span class="absolute"></span></button>
+						<button title="ajouter bf3 aux favoris" class="love-btn absolute"><span class="absolute"></span></button>
+					</div>
+				</article>
+			</div>
+		</div>
+	</section>
+
+	<div class="tournoi-separator">
+		<div class="line-sep"></div>
+		<div class="point-sep"></div>
+		<div class="line-sep"></div>
+	</div>
+
+	<section class="ta-center tournoi-choix-date">
+		<div class="max-container">
+			<h2 class="titre2">Tournois par date</h2>
+			<div class="tri-calendrier">
+				<div class="tri-jours">
+					<label for="jour">Jours</label>
+					<select name="jour">
+						<option selected value="none">tri par jour</option>
+						<option value="j1">jour j+1</option>
+						<option value="j2">jour j+2</option>
+						<option value="j3">jour j+3</option>
+						<option value="j4">jour j+4</option>
+						<option value="j5">jour j+5</option>
+						<option value="j6">jour j+6</option>
+					</select>
+				</div>
+				<div class="tri-semaines">
+					<label for="semaine">Semaines</label>
+					<select name="semaine">
+						<option selected value="none">tri par mois</option>
+						<option value="n1">semaine n+1</option>
+						<option value="n2">semaine n+2</option>
+						<option value="n3">semaine n+3</option>
+					</select>
+				</div>
+				<div class="tri-date-actif tri-mois">
+					<label for="mois">Mois</label>
+					<select name="mois">
+						<option value="none">tri par mois</option>
+						<option value="m1">mois m+1</option>
+						<option selected value="m2">mois m+2</option>
+						<option value="m3">mois m+3</option>
+					</select>
+				</div>
+			</div>
+			<div class="prochains-tournois-tries">
+				<table class="table-triee">
+					<thead>
+						<tr>
+							<th>Jeu</th>
+							<th>Orga</th>
+							<th>Console</th>
+							<th>Ouvert</th>
+							<th>Joueurs-max</th>
+							<th>Equipe</th>
+							<th class="tri-actif">Date</th>
+						</tr>
+					</thead>					
+					<tbody>
+						<tr>							
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>ouvert</td>
+							<td>32</td>
+							<td>equipe-8</td>
+							<td class="colonne-triee">15/29/03</td>
+						</tr>
+						<tr>
+							<td>CSS</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>sur demande</td>
+							<td>10</td>
+							<td>equipe-5</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PS3</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>equipe-16</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>LOL</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>invitation</td>
+							<td>10</td>
+							<td>equipe-5-auto</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>SSB</td>
+							<td>pseudo</td>
+							<td>N64</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>individuel</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>FIFA16</td>
+							<td>pseudo</td>
+							<td>XboxOne</td>
+							<td>ouvert</td>
+							<td>16</td>
+							<td>equipe-2</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>guilde-only</td>
+							<td>48</td>
+							<td>equipe-4</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>LOL</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>ouvert</td>
+							<td>10</td>
+							<td>equipe-5-libre</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>CSS</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>invitation-only</td>
+							<td>16</td>
+							<td>equipe-8-auto</td>
+							<td class="colonne-triee">15/25/03</td>
+						</tr>
+						<tr>
+							<td>BF3</td>
+							<td>pseudo</td>
+							<td>PC</td>
+							<td>guilde-only</td>
+							<td>32</td>
+							<td>equipe-8</td>
+							<td class="colonne-triee">16/25/03</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr>
+							<th>Jeu</th>
+							<th>Orga</th>
+							<th>Console</th>
+							<th>Ouvert</th>
+							<th>Joueurs-max</th>
+							<th>Equipe</th>
+							<th>Date</th>
+						</tr>
+					</tfoot>
+				</table>
+			</div>
+		</div>
+	</section>
+</div>	
+<script>
+function getScrollTop(){
+    if(typeof pageYOffset!= 'undefined'){
+        //most browsers except IE before #9
+        return pageYOffset;
+    } else {
+        var B = document.body; //IE 'quirks'
+        var D = document.documentElement; //IE with doctype
+        D = (D.clientHeight) ? D : B;
+        return D.scrollTop;
+    }
+}
+
+var timeouts = [];
+var scrolling = false;
+var scroller;
+var scrollTop = getScrollTop();
+var timeMs;
+var alter = false;
+var speed = 5;
+document.addEventListener('scroll', function() {
+    if(alter) {
+        var timeDif = new Date().getMilliseconds() - timeMs;
+        speed = 5 - (timeDif / 50);
+        console.log(speed);
+    }
+    timeMs = new Date().getMilliseconds();
+    alter = !alter;
+    var scrollDirection = getScrollTop() - scrollTop;
+    scrollDirection = scrollDirection / Math.abs(scrollDirection);
+    scrollTop = getScrollTop();
+    clearTimeout(scroller);
+    scroller = setTimeout(function(){
+        console.log('smooth scrolling active');
+        if(!scrolling) {
+            timeouts.length = 0;
+            scrolling = true;
+            var steps = 50;
+            var delay = 6;
+            for(var i = 0; i < steps; i++) {
+                (function(i){
+                    var timeout = setTimeout(function(){
+                        var perc = i / steps; 
+                        var val = (perc == 1) ? 1 : (-Math.pow(2, -10 * perc) + 1); 
+                        var scrollY = val * speed * scrollDirection;
+                        window.scrollTo(0, getScrollTop() + scrollY);
+                        setTimeout(function(){
+                            if(i == (steps - 1)) scrolling = false;
+                        }, steps * delay);
+                    }, (i * delay));
+                    timeouts.push(timeout);
+                })(i);
+            }
+        }
+    }, 50);
+});
+</script>
