079df020536da5f76c371d4ed6354660b83cbde1
dydyFos <YOUR EMAIL ADRESS>
2016-02-22 01:53:49 +0100

    Création de la page "detailtournoi" - premier draft

---
+++ views/detailtournoiDOM.php
@@ -0,0 +1,117 @@
+<div class="max-container">
+	<section class="detailtournoi-infos flex">
+		<aside class="ta-center relative">
+			<img src="web/img/bf3-bg.jpg" alt="Battlefield 3">
+			<figcaption class="ta-center italic">Pour les gamers sur PC seulement !</figcaption>
+			<div class="relative ta-right">
+				<button class="detailtournoi-btn-inscription relative"><a></a></button>
+			</div>
+		</aside>
+		<article>
+			<div class="ta-right">
+				<h3 class="titre1">Tournoi - <span class="uppercase">battlefield 3</span></h3>
+			</div>
+			<p class="detailtournoi-description-jeu italic">L'un des tout meilleurs FPS sur PC. Plongez dans un conflit moderne qui embrase les 4 coins du monde. Avec ses 4 classes customisables, ses armes modifiables, ses nombreux véhicules et son système d'escouade, il s'oriente principalement vers le jeu d'équipe.</p>
+			<div class="detailtournoi-tournoi-regles flex">
+				<p class="relative detailtournoi-jeu-mode">Mode:<span class="relative">Ruée par équipe<i class="absolute ta-center">jouez en équipe pour attaquer et défendre par tour des points</i></span>
+				</p>
+				<p class="relative detailtournoi-jeu-console">Console: <span>PC</span></p>
+				<p class="relative detailtournoi-jeu-online">Où: <span>online</span></p>
+				<p class="relative detailtournoi-jeu-maxjoueurs">Joueurs max: <span>32 joueurs</span></p>
+				<p class="relative detailtournoi-jeu-minjoueurs">Joueurs min: <span>24 joueurs</span></p>
+				<p class="relative detailtournoi-jeu-victoire">Règles: <span>match de 3 manches</span></p>
+				<p class="relative detailtournoi-jeu-reglesEquipe">Equipes: <span>fixes - random</span></p>
+				<p class="relative detailtournoi-jeu-date">Quand : <span>Le 22/03/2015 - 18h00</span></p>
+				<p class="relative detailtournoi-jeu-organisateur">Orga : <span>Pseudo</span></p>
+			</div>			
+		</article>
+	</section>
+	<section class="detailtournoi-participants">
+		<h2 class="titre2">Les participants <span class="detailtournoi-nombre-participants">(16)</span></h2>
+		<div class="flex detailtournoi-liste-participants">
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">Fyjal<span class="absolute detailtournoi-stats-joueur">87 victoires, 51%win</span></p>
+				<p class="detailtournoi-participant-points absolute">1200</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">dRowiid<span class="absolute detailtournoi-stats-joueur">65 victoires, 49%win</span></p>
+				<p class="detailtournoi-participant-points absolute">955</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>	
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">Rcan<span class="absolute detailtournoi-stats-joueur">90 victoires, 13%win</span></p>
+				<p class="detailtournoi-participant-points absolute">892</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">ooBsidian<span class="absolute detailtournoi-stats-joueur">112 victoires, 9%win</span></p>
+				<p class="detailtournoi-participant-points absolute">865</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">GWlord<span class="absolute detailtournoi-stats-joueur">13 victoires, 100%win</span></p>
+				<p class="detailtournoi-participant-points absolute">623</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">milky<span class="absolute detailtournoi-stats-joueur">58 victoires, 67%win</span></p>
+				<p class="detailtournoi-participant-points absolute">601</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">anyTi<span class="absolute detailtournoi-stats-joueur">12 victoires, 100%win</span></p>
+				<p class="detailtournoi-participant-points absolute">522</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">supYo<span class="absolute detailtournoi-stats-joueur">45 victoires, 68%win</span></p>
+				<p class="detailtournoi-participant-points absolute">431</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">dHYdo<span class="absolute detailtournoi-stats-joueur">76 victoires, 25%win</span></p>
+				<p class="detailtournoi-participant-points absolute">431</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">comeAgn<span class="absolute detailtournoi-stats-joueur">50 victoires, 51%win</span></p>
+				<p class="detailtournoi-participant-points absolute">426</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">headsRoll<span class="absolute detailtournoi-stats-joueur">19 victoires, 95%win</span></p>
+				<p class="detailtournoi-participant-points absolute">403</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">n_e_day<span class="absolute detailtournoi-stats-joueur">10 victoires, 42%win</span></p>
+				<p class="detailtournoi-participant-points absolute">386</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">i_c_u<span class="absolute detailtournoi-stats-joueur">2 victoires, 90%win</span></p>
+				<p class="detailtournoi-participant-points absolute">374</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">pbblyNot<span class="absolute detailtournoi-stats-joueur">32 victoires, 12%win</span></p>
+				<p class="detailtournoi-participant-points absolute">327</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">irdc<span class="absolute detailtournoi-stats-joueur">5 victoires, 3%win</span></p>
+				<p class="detailtournoi-participant-points absolute">308</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+			<div class="detailtournoi-participant relative flex">
+				<p class="detailtournoi-participant-pseudo">rtfm<span class="absolute detailtournoi-stats-joueur">21 victoires, 18%win</span></p>
+				<p class="detailtournoi-participant-points absolute">308</p>
+				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
+			</div>
+		</div>
+	</section>
+	<section class="detailtournoi-bracket">
+		<h2 class="titre2">Resultats des rounds - Bracket</h2>
+	</section>
+</div>
