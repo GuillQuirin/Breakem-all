0025e3267a9fd5e050a0b2dd0b3cf2c166696e06
dydyFos <YOUR EMAIL ADRESS>
2016-02-23 20:45:34 +0100

    DOM - commentaires en place

--- views/detailtournoiDOM.php
+++ views/detailtournoiDOM.php
@@ -8,7 +8,7 @@
 			</div>
 		</aside>
 		<article>
-			<div class="ta-right">
+			<div class="ta-center">
 				<h3 class="titre1">Tournoi - <span class="uppercase">battlefield 3</span></h3>
 			</div>
 			<p class="detailtournoi-description-jeu italic">L'un des tout meilleurs FPS sur PC. Plongez dans un conflit moderne qui embrase les 4 coins du monde. Avec ses 4 classes customisables, ses armes modifiables, ses nombreux véhicules et son système d'escouade, il s'oriente principalement vers le jeu d'équipe.</p>
@@ -28,91 +28,170 @@
 		</article>
 	</section>
 	<section class="detailtournoi-participants">
-		<h2 class="titre2">Les participants <span class="detailtournoi-nombre-participants">(16)</span></h2>
+		<h2 class="titre2">Participants <span class="detailtournoi-nombre-participants">(16)</span></h2>
 		<div class="flex detailtournoi-liste-participants">
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">Fyjal<span class="absolute detailtournoi-stats-joueur">87 victoires, 51%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>Fyjal</a><span class="absolute detailtournoi-stats-joueur">87 victoires, 51%win</span></p>
 				<p class="detailtournoi-participant-points absolute">1200</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">dRowiid<span class="absolute detailtournoi-stats-joueur">65 victoires, 49%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>dRowiid</a><span class="absolute detailtournoi-stats-joueur">65 victoires, 49%win</span></p>
 				<p class="detailtournoi-participant-points absolute">955</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>	
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">Rcan<span class="absolute detailtournoi-stats-joueur">90 victoires, 13%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>Rcan</a><span class="absolute detailtournoi-stats-joueur">90 victoires, 13%win</span></p>
 				<p class="detailtournoi-participant-points absolute">892</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">ooBsidian<span class="absolute detailtournoi-stats-joueur">112 victoires, 9%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>ooBsidian</a><span class="absolute detailtournoi-stats-joueur">112 victoires, 9%win</span></p>
 				<p class="detailtournoi-participant-points absolute">865</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">GWlord<span class="absolute detailtournoi-stats-joueur">13 victoires, 100%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>GWlord</a><span class="absolute detailtournoi-stats-joueur">13 victoires, 100%win</span></p>
 				<p class="detailtournoi-participant-points absolute">623</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">milky<span class="absolute detailtournoi-stats-joueur">58 victoires, 67%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>milky</a><span class="absolute detailtournoi-stats-joueur">58 victoires, 67%win</span></p>
 				<p class="detailtournoi-participant-points absolute">601</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">anyTi<span class="absolute detailtournoi-stats-joueur">12 victoires, 100%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>anyTi</a><span class="absolute detailtournoi-stats-joueur">12 victoires, 100%win</span></p>
 				<p class="detailtournoi-participant-points absolute">522</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">supYo<span class="absolute detailtournoi-stats-joueur">45 victoires, 68%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>supYo</a><span class="absolute detailtournoi-stats-joueur">45 victoires, 68%win</span></p>
 				<p class="detailtournoi-participant-points absolute">431</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">dHYdo<span class="absolute detailtournoi-stats-joueur">76 victoires, 25%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>dHYdo</a><span class="absolute detailtournoi-stats-joueur">76 victoires, 25%win</span></p>
 				<p class="detailtournoi-participant-points absolute">431</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">comeAgn<span class="absolute detailtournoi-stats-joueur">50 victoires, 51%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>comeAgn</a><span class="absolute detailtournoi-stats-joueur">50 victoires, 51%win</span></p>
 				<p class="detailtournoi-participant-points absolute">426</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">headsRoll<span class="absolute detailtournoi-stats-joueur">19 victoires, 95%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>headsRoll</a><span class="absolute detailtournoi-stats-joueur">19 victoires, 95%win</span></p>
 				<p class="detailtournoi-participant-points absolute">403</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">n_e_day<span class="absolute detailtournoi-stats-joueur">10 victoires, 42%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>n_e_day</a><span class="absolute detailtournoi-stats-joueur">10 victoires, 42%win</span></p>
 				<p class="detailtournoi-participant-points absolute">386</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">i_c_u<span class="absolute detailtournoi-stats-joueur">2 victoires, 90%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>i_c_u</a><span class="absolute detailtournoi-stats-joueur">2 victoires, 90%win</span></p>
 				<p class="detailtournoi-participant-points absolute">374</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">pbblyNot<span class="absolute detailtournoi-stats-joueur">32 victoires, 12%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>pbblyNot</a><span class="absolute detailtournoi-stats-joueur">32 victoires, 12%win</span></p>
 				<p class="detailtournoi-participant-points absolute">327</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">irdc<span class="absolute detailtournoi-stats-joueur">5 victoires, 3%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>irdc</a><span class="absolute detailtournoi-stats-joueur">5 victoires, 3%win</span></p>
 				<p class="detailtournoi-participant-points absolute">308</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 			<div class="detailtournoi-participant relative flex">
-				<p class="detailtournoi-participant-pseudo">rtfm<span class="absolute detailtournoi-stats-joueur">21 victoires, 18%win</span></p>
+				<p class="detailtournoi-participant-pseudo"><a>rtfm</a><span class="absolute detailtournoi-stats-joueur">21 victoires, 18%win</span></p>
 				<p class="detailtournoi-participant-points absolute">308</p>
 				<button class="detailtournoi-btn-fichejoueur relative"><a></a></button>
 			</div>
 		</div>
 	</section>
-	<section class="detailtournoi-bracket">
+	<!-- <section class="detailtournoi-bracket">
 		<h2 class="titre2">Resultats des rounds - Bracket</h2>
+	</section> -->
+	<section class="detailtournoi-commentaires">
+		<h2 class='titre2'>Commentaires</h2>
+		<div class="detailtournoi-container-commentaires">
+			<div class="relative detailtournoi-commentaire">
+				<div>
+					<h5>16/02 09:08</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>dRowiid</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Futura quidem sed Mihi hodie qualis autem futurum ire coepit Mihi sed inferentem quis supplicio.</p>
+				</div>
+			</div>
+			<div class="relative detailtournoi-commentaire">
+				<div>
+					<h5>Lun 02:00</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>Fyjal</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Futura quidem sed Mihi hodie qualis autem futurum ire coepit Mihi sed inferentem quis supplicio.</p>
+				</div>
+			</div>
+			<div class="relative detailtournoi-commentaire">
+				<div>
+					<h5>Lun 03:12</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>Ypsos</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Valet amicitiam ad comparandis ferendum potius Quin ad praeceptum quem amicitiam adhiberemus amare ferendum amicitiis.</p>
+				</div>
+			</div>
+			<div class="relative detailtournoi-commentaire">
+				<div>
+					<h5>Lun 03:17</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>OopsIdian</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Saeculis Damascus quibus Damascus et monti urbibus Berytus Emissa magnis Sidon celebritateque quibus quibus adclinis.</p>
+				</div>
+			</div>
+			<div class="relative detailtournoi-commentaire detailtournoi-commentaire-current-user">
+				<div>
+					<h5>Mar 19:17</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>Toi</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Saeculis Damascus quibus Damascus et monti urbibus Berytus Emissa magnis Sidon celebritateque quibus quibus adclinis.</p>
+				</div>
+			</div>
+			<div class="relative detailtournoi-commentaire">
+				<div>
+					<h5>Mar 20:12</h5>
+				</div>
+				<div class="detailtournoi-commentaire-pseudo">
+					<p><a>RxR_d</a></p>
+				</div>
+				<div class="detailtournoi-commentaire-userpick">
+					<p class="detailtournoi-message">Constantio aetatis e atque praefecturae consulares inmaturo et quadriennio ipse.</p>
+				</div>
+			</div>
+		</div>
+		<div class="detailtournoi-container-post-commentaire">
+			<div class="detailtournoi-post-container">
+				<form  method="post">
+					<textarea name="commentaire-post" cols="30" rows="10" placeholder"Votre message ici"></textarea>
+					<input type="submit" value="" hidden>
+				</form>
+			</div>
+		</div>
 	</section>
 </div>
