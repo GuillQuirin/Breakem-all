e1275c85e7dc9ede4b5edaf4ba8297f3c7103108
Meghan Wlo <meghan.wlodarczyk@outlook.com>
2016-02-28 16:40:27 +0100

    Liste tournoi

--- views/index.php
+++ views/index.php
@@ -2,9 +2,8 @@
 
 <div id="header">
 	<div id="header_width">
-		<div id="title">
+		<div id="nom_site">
 			<h2> Break'em All </h2>
-			 
 		</div>
 		<div id="header_right">
 			<button type="submit" id="btn_connexion" class="bouton">Connexion</a>
@@ -23,7 +22,6 @@
 	<div id="inscription_rapide">
 		<br>
 		<h3>Inscris toi et participe à des<br> tournois seul ou en Equipe !<br></h3>
-		<hr>
 		<p> Inscription rapide : </p>
 		<input id="email" type="text" placeholder="E-mail">
 		<input id="pwd1" type="text" placeholder="Rentre ton mot de passe"> 
@@ -33,7 +31,30 @@
 		</div>
 	</div>
 </section>
-<div class="separateur"></div>
+
+<div class="separateur">
+	<div id="title">
+		<hr class="left"> 
+		<div class="title">Les tournois</div>
+		<hr class="right">
+	</div>
+</div>
+
+
 <section id="section_liste_tournoi">
-	
+	<article id="liste_tournoi">
+		<div class="tournoi_accueil">
+			<div class="border_img_tournoi">
+				<span class="helper"></span>
+				<img src="web/img/mk.jpg" alt="Mario Kart" title="Mario Kart" class="img_tournoi">
+			</div>
+			<div class="descrip_tournoi">
+				<div class="descrip_icon">
+					
+				</div>
+				<h4> Tournoi Mario Kart </h4>,Mardi 09 Février 2016
+				<p>  </p>
+			</div>
+		</div>
+	</article>
 </section>
