445ad13457ac1f5e137b9060c0c778bdf1587139
Meghan Wlo <meghan.wlodarczyk@outlook.com>
2016-03-02 16:01:47 +0100

    Test serveur hooks

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	include "vue/template/header.php";
 ?>
 
-<p> Vous êtes bien sur le site Break'em all </p>
+<p> Vous êtes bien sur le site Break'em allllll </p>
 <?php
 	include "vue/template/footer.php";
 ?>
