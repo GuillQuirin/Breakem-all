5354b718a982aee471101ccc1f8075b58586416c
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-03-06 15:33:47 +0100

    Architecture + css de la page publique PROFIL

--- views/profil.php
+++ views/profil.php
@@ -1,55 +1,76 @@
 	<section id="presentation">
 		<div id="image">
-			<figure>
-				<a href="">Changer l'image</a>
-				<img src="mlpp.jpg">
-				<figcaption>
-					PSEUDO
-				</figcaption>
-			</figure>
-		</div>
-		<div id="description">
-			<header id="headdesc">
-				<a href="">Modifier</a>
-			</header>
-			<p id="signature">"Petite signature ou texte descriptif"</p>
-			<p id="localisation">Localisation: Paris </p>
-			<p id="age">Age: 15 ans </p>
-			<img src="sx.jpg" id="sexe">
-			<p id="score">Score: 15000 points</p>
-			<p id="team">Team: Pas de team</p>
+			<img src="" alt="logo">
+			<p>PSEUDO</p>
+			<p>Dernière connexion: </p>
 		</div>
 	</section>
-	<section id="stats">
-		<div id="participation">
-			<table>
-				<thead>
-					<th>Derniers tournois joués:</th>
-					<th>Classement</th>
-				</thead>
-				<tbody>
-					<tr><td>-</td><td>1er</td></tr>
-					<tr><td>-</td><td>1er</td></tr>
-					<tr><td>-</td><td>1er</td></tr>
-					<tr><td>-</td><td>1er</td></tr>
-				</tbody>
-			</table>
+	<section id="description">
+		<div id="communication">
+			<button>Contacter</button>
+			<button>/!\</button>
 		</div>
-		<div id="organisation">
-			<table>
-				<thead>
-					<th>Derniers tournois organisés:</th>
-				</thead>
-				<tbody>
-					<tr><td>-</td></tr>
-					<tr><td>-</td></tr>
-					<tr><td>-</td></tr>
-					<tr><td>-</td></tr>
-				</tbody>
-			</table>
+		<div id="statistiques">
+			<div>
+				<h4>Description</h4>
+				<p>Ce joueur n'a pas encore rédigé de description.
+				</p>
+			</div>
+			<div>
+				<h4>Statistiques</h4>
+				<ul>
+					<li>Matchs gagnés: 78
+					<li>Matchs perdus: 92
+					<li>Ratio : 0.78
+					<li>Points : 1072
+				</ul>
+			</div>
 		</div>
 	</section>
-	<section id="buttons">
-		<button>Contacter le joueur</button>
-		<button>Signaler le joueur</button>
+	<section id="tableaux">
+		<div>
+			<h4>Derniers tournois joués</h4>
+			<ul>
+				<li>
+					<a href="#">Nom Tournoi</a>
+					<span>Jeux</span>  
+					<span>Position</span>  
+				<li>
+					<a href="#">Nom Tournoi</a>
+					<span>Jeux</span>  
+					<span>Position</span>
+				<li>
+					<a href="#">Nom Tournoi</a>
+					<span>Jeux</span>  
+					<span>Position</span>
+			</ul>
+		</div>
+		<div>
+			<h4>Jeux favoris</h4>
+			<ul>
+				<li>
+					<a href="#"><img src="" alt="" title=""></a>  
+				<li>
+					<a href="#"><img src="" alt="" title=""></a>
+				<li>
+					<a href="#"><img src="" alt="" title=""></a>
+			</ul>
+		</div>
+		<div>
+			<h4>Derniers matchs</h4>
+			<ul>
+				<li>
+					<a href="#">Adversaire</a>
+					<span>Jeux</span>  
+					<span class="win">Victoire</span>  
+				<li>
+					<a href="#">Adversaire</a>
+					<span>Jeux</span>  
+					<span class="loo">Defaite</span>
+				<li>
+					<a href="#">Adversaire</a>
+					<span>Jeux</span>  
+					<span class="win">Victoire</span>
+			</ul>
+		</div>
 	</section>
