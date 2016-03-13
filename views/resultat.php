b56c30107c12830fe54297767ede97e2e5965263
unknown <ventialaw@hotmail.fr>
2016-03-13 22:34:44 +0100

    Avatar test

--- views/resultat.php
+++ views/resultat.php
@@ -1,13 +1,34 @@
 <link rel="stylesheet" href="web/css/general-stylesheet.css">
 <link rel="stylesheet" href="web/css/module/grid.css">
 <link rel="stylesheet" href="web/css/module/size.css">
-<div class="full-height">
-	<div class="container resultat-container m-a bg-cover">
-		
+<link rel="stylesheet" href="web/css/module/text.css">
+<section class="full-height">
+	<div class="container resultat-container m-a bg-cover text-center">
+		<div class="full-height resultat-center-v">
+			<!--<div class="grid-md-6">
+				<div class="grid-md-12">	
+					<img class="resultat-avatar" src="web/img/avatar1.jpg">
+				</div>
+				<div class="grid-md-6">
+					<img class="resultat-avatar" src="web/img/avatar2.png">
+				</div>
+				<div class="grid-md-6">
+					<img class="resultat-avatar" src="web/img/avatar3.jpg">
+				</div>
+			</div>
+			<div class="grid-md-6">
+				<div class="grid-md-12">
+
+				</div>
+				<div class="grid-md-6">
+
+				</div>
+				<div class="grid-md-6">
+				
+				</div>
+
+			</div>
+			-->
+		</div>
 	</div>
-</div>
-<div class="full-height">
-	<div class="container resultat-container m-a bg-cover">
-		
-	</div>
-</div>
+</section>
