1e95b7ec40f79d25b6e7905f20b55e7c9fb76da2
Meghan Wlo <meghan.wlodarczyk@outlook.com>
2016-03-14 14:53:20 +0100

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once "conf.inc.php";
 
 // Reloader automatique + Jolie comm
-function mon_loader($class){
+function vgvgvgmon_loader($class){
 	if( file_exists("core/".$class.".class.php")){
 		require_once("core/".$class.".class.php");
 	}
