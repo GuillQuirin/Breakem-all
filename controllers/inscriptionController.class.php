94e03e27ee7ca815ceca29faa8880e9bce61d1ef
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-03-16 20:00:12 +0100

    Mise en place du controle des inputs

--- controllers/inscriptionController.class.php
+++ controllers/inscriptionController.class.php
@@ -12,7 +12,41 @@ class inscriptionController{
 	}
 
 	public function verifyAction(){
-		var_dump($_POST);
-	}
-	
+		$args = array(
+		    'pseudo'     => FILTER_SANITIZE_STRING,
+		    'email'   => FILTER_VALIDATE_EMAIL,
+		    'password'   => FILTER_SANITIZE_STRING,
+		    'password_check'   => FILTER_SANITIZE_STRING,
+		    'day'   => FILTER_VALIDATE_INT,	    
+		    'month'   => FILTER_VALIDATE_INT,	    
+		    'year'   => FILTER_VALIDATE_INT	    
+		);
+		$filteredinputs = filter_input_array(INPUT_POST, $args);
+
+		foreach ($args as $key => $value) {
+			if(!isset($filteredinputs[$key]))
+				die("FAUX: ".$filteredinputs[$key]);
+		}
+
+		$email = $filteredinputs['email'];
+
+		if(strlen($filteredinputs['pseudo'])<2 || strlen($filteredinputs['pseudo'])>45)
+        	die("FAIL pseudo");	
+        else
+ 	       $pseudo=trim($filteredinputs['pseudo']);
+
+        if($filteredinputs['password']!==$filteredinputs['password_check'])
+        	die("FAIL pwd");
+        else
+        	$pwd=password_hash($filteredinputs['password'], PASSWORD_DEFAULT);
+
+        if(!checkdate($filteredinputs['month'], $filteredinputs['day'], $filteredinputs['year']))
+        	die("FAIL date crea");
+        else{
+        	$date = DateTime::createFromFormat('j-n-Y',$filteredinputs['day'].'-'.$filteredinputs['month'].'-'.$filteredinputs['year']);
+        	if(!$date)
+        		die("FAIL date format");
+        	$datebdd=date_timestamp_get($date);
+        }
+        //$datebdd / $pwd / $email / $pseudo
 }
