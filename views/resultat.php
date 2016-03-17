058746a32046540cbb87da48b075942c723472ad
unknown <ventialaw@hotmail.fr>
2016-03-17 16:59:24 +0100

    no message

--- views/resultat.php
+++ views/resultat.php
@@ -2,8 +2,7 @@
 	<div class="full-width m-a bg-cover text-center">
 		<div class="full-height resultat-center-v">
 			<div class="container m-a">
-				<div class="grid-md-12">
-					
+				<div class="grid-md-12" style="margin-top:35px;">
 				</div>
 			</div>
 		</div>
