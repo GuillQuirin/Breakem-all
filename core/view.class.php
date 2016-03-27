5fac75db2c71dc291c5d4b9c5e513dbf9d055261
dydyFos <YOUR EMAIL ADRESS>
2016-03-27 19:39:34 +0200

    modif du template par defaut a "templatenew" en lieu et place de "template" dans view.class.php

--- core/view.class.php
+++ core/view.class.php
@@ -6,7 +6,7 @@ class View{
 	protected $view;
 	protected $template;
 
-	public function setView($view, $layout="template"){
+	public function setView($view, $layout="templatenew"){
 		$path_view = "views/".$view.".php";
 		$path_template = "views/".$layout.".php";
 
