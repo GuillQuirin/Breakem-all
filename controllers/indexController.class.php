2bb368fd9532b00754d340a749ee2784a9413bc6
Dylan <dylanfoster.esgi@gmail.com>
2016-04-19 21:41:39 +0200

    poursuite des test de connexion ajax avec création token

--- controllers/indexController.class.php
+++ controllers/indexController.class.php
@@ -10,25 +10,55 @@ class indexController extends template{
 	}
 
 	public function connectionAction(){
+		if(isset($_COOKIE['breakemalltoken'])){
+			print_r($_COOKIE);
+			return;
+		}
+		echo "cookie was not found ! \n";
+		unset($_COOKIE['breakemalltoken']);
+		return;
 		$args = array(
     	    'email'   => FILTER_VALIDATE_EMAIL,
     	    'password'   => FILTER_SANITIZE_STRING 
 		);
 		$filteredinputs = filter_input_array(INPUT_POST, $args);
 
+		// Array final à encoder en json
+		$data = [];
+
 		$requiredInputsReceived = true;
 		foreach ($args as $key => $value) {
 			if(!isset($filteredinputs[$key])){
-				$requiredInputsReceived = false;
-				break;
+				$requiredInputsReceived = false;				
+				$data["errors"]["inputs"] = "missing input " . $key;
 			}
 		}
 
 		if($requiredInputsReceived){
-			print_r($filteredinputs);
+			$userManager = new userManager();
+			$user = new user($filteredinputs);
+			if($userManager->userMailExists($user)){
+				$dbUser = $userManager->tryConnect($user);
+				if(!!$dbUser){
+					echo "connection succes ! \n";
+					unset($_SESSION);
+					$token = md5($dbUser->getId().$dbUser->getName().$dbUser->getEmail().SALT.date('Ymd'));
+					$_SESSION['token'] = $token;
+					setcookie('breakemalltoken', $token, time()+(86400 * 7));
+					unset($token);
+					print_r($_SESSION);
+					$data["pseudo"] =$dbUser->getPseudo();
+					$data["email"] = $dbUser->getEmail();
+				}else{
+					$data["errors"]["user"] = "invalid password !";
+				}
+			}else{
+				$data["errors"]["user"] = "unknown email !";
+			}
 		}
-		else{
-			echo "missing an input ! \n";
-		}
+
+		echo json_encode($data);
 	}
 }
+
+/*5fc88f953172a14a9de09615a5789a1a*/
