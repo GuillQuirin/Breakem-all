54a8ca49f5739c99ea70b754e92ab43f01c52d26
Teddy Meksavanh <teddy@mobilispro.com>
2016-04-25 15:35:40 +0200

    Résultat debut no hate

--- views/resultat.php
+++ views/resultat.php
@@ -1,10 +1,18 @@
-<section>
-	<div class="full-width bg-blue">
-		<?php echo '<img src="' . WEBPATH . '/web/img/header.jpg">';?>
-		<div class="">
-			<h1><a>Le Meilleur se cache parmi vous !</a></h1>
+<section class="middle-height bg-cover">
+		
+		<div class="align full-height">
+			<span class="resultat-header-title border-full">Classement</span>
+			<div class="resultat-header-rank">
+
+			</div>
+			<div class="resultat-header-rank">
+				
+			</div>
+			<div class="resultat-header-rank">
+				
+			</div>
 		</div>
-	</div>
+	
 </section>
 
 
