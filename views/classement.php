cc9df95e4e364678296dc4cad9b8ab98b3881b0b
unknown <ventialaw@hotmail.fr>
2016-04-26 00:58:55 +0200

    Classement no inspiration lmao

--- views/classement.php
+++ views/classement.php
@@ -18,38 +18,86 @@
 </section>
 <section class="classement-content-wrapper">
 
-	<div class="container full-width">
+	<div class="container m-a content-border classement-container">
 
-		<div class="row">
-			<div class="grid-md-8 classement-content-main">
+		<div class="row classement-content-row">
+			<div class="grid-md-12">
 
-				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
-					<div class="classement-header-ranking-wrapper">
-						<img src="web/img/heroes-of.jpg" class="img-cover">
+				<div class="classement-nav-hori-wrapper grid-md-12">
+					
+					<div class="nav_hori">
+						<ul>
+							<li class="border_menu active_menu"><a href="#">Equipe</a></li>
+							<li class="border_menu"><a href="#">Solo</a></li>
+							<li class="border_menu"><a href="#">5vs5</a></li>
+							<li class="border_menu"><a href="#">2vs2</a></li>
+							<li class="border_menu"><a href="#">Plus...</a></li>
+						</ul>
+					</div>
+
+					<div class="classement-select-hori-wrapper">
+						<ul>
+							<li>						
+								<div>
+									<label class="classement-select-hori-label">Trier par Jeux :</label>
+									<select class="classement-select-hori">
+										<option>Tout</option>
+										<option>Super Smash Bros Melee</option>
+										<option>Mario Kart</option>
+										<option>Heroes of the Storm</option>
+									</select>
+								</div>
+							</li>
+						</ul>
+					</div>
+				
+				</div>
+
+				<!-- classement -->
+				<div class="grid-md-6 classement-resultat-wrapper">
+					<div class="grid-md-6 grid-md-offset-3 classement-resultat">				
+						<img src="web/img/heroes-of.jpg" class="img-cover">	
+						<span class="classement-ranking-icon">1</span>	
+					</div>
+					<div class="grid-md-6">
+						<div class="classement-resultat">
+							<img src="web/img/heroes-of.jpg" class="img-cover">	
+							<span class="classement-ranking-icon">2</span>	
+						</div>	
+					</div>
+					<div class="grid-md-6">
+						<div class="classement-resultat">
+							<img src="web/img/heroes-of.jpg" class="img-cover">
+							<span class="classement-ranking-icon">3</span>		
+						</div>		
 					</div>
 				</div>
-				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
-					<div class="classement-header-ranking-wrapper">
-						<img src="web/img/heroes-of.jpg" class="img-cover">		
+				<!-- f classement -->
+
+				<!-- classement -->
+				<div class="grid-md-6 classement-resultat-wrapper">
+					<div class="grid-md-6 grid-md-offset-3 classement-resultat">				
+						<img src="web/img/heroes-of.jpg" class="img-cover">	
+						<span class="classement-ranking-icon">1</span>	
 					</div>
-				</div>			
+					<div class="grid-md-6">
+						<div class="classement-resultat">
+							<img src="web/img/heroes-of.jpg" class="img-cover">	
+							<span class="classement-ranking-icon">2</span>	
+						</div>	
+					</div>
+					<div class="grid-md-6">
+						<div class="classement-resultat">
+							<img src="web/img/heroes-of.jpg" class="img-cover">
+							<span class="classement-ranking-icon">3</span>		
+						</div>		
+					</div>
+				</div>
+				<!-- f classement -->
 
 			</div>
 
-			<div class=" grid-md-4 classement-content-navbar-side">
-
-				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
-					<div class="classement-header-ranking-wrapper">
-						<span class="classement-header-ranking">1</span>		
-					</div>
-				</div>
-				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
-					<div class="classement-header-ranking-wrapper">
-						<span class="classement-header-ranking">1</span>		
-					</div>
-				</div>	
-
-			</div>
+		
 		</div>
 
 	</div>
