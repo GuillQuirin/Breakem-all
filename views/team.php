792aa97715b90b0601f57fcfaefd941568d63a82
Rémy Cariou <Rmy Cariou>
2016-04-25 11:22:39 +0200

    no message

--- views/team.php
+++ views/team.php
@@ -1,18 +1,20 @@
-<section id="name">
-    <div id="name1">TEAM</div>
-    <img id="Img" src="web/img/SF4B.jpg" />
-    <div id="name2">BREAK</div>
+<section id ="up">
+    <div id ="onglet1">
+
+    </div>
+
+    <div id ="onglet2">
+
+    </div>
+
+    <div id ="onglet3">
+
+    </div>
 </section>
-<section id ="Info"><div id ="Info1">Rayz<br>
-        Joueur Smash bros et Street fighter<br>
-        10 victoire - 4 défaites<br>
-        Team Break <br></div></section>
-<section id ="Desc"><div id ="Desc1">Description de la team <br><br> Team Break en projet annuel pour le site break em all destiné à la création de tournoi</div></section>
-<section id ="List"><div id ="List1">Liste des membres de la team:<br><br> Meghan <br><br> Dylan <br><br> Guillaume <br><br> Teddy <br><br> Remy</div></section>
-<div><img id="Balrog" src="web/img/Balrog-gigaton.gif" />
-    <button id ="Quit">Quitter le groupe</button></div>
 <footer>
 </footer>
+
+<script src="jquery.js"></script>
 
 </body>
 </html>
