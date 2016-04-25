007fbcfddb292c6f136b90ccd37952342b2aad61
Teddy Meksavanh <teddy@mobilispro.com>
2016-04-25 17:51:08 +0200

    Classement debut

---
+++ views/classement.php
@@ -0,0 +1,27 @@
+<section class="middle-height bg-cover relative">
+	
+	<div class="align full-height">
+		<span class="classement-header-title border-full">Classement</span>
+		<div class="classement-header-ranking-wrapper">
+			<span class="classement-header-ranking">1</span>		
+		</div>
+		<div class="classement-header-ranking-wrapper">
+			<span class="classement-header-ranking">2</span>
+		</div>
+		<div class="classement-header-ranking-wrapper">
+			<span class="classement-header-ranking">3</span>
+		</div>
+	</div>
+	 
+	<img class="icon icon-size-3 down-center classement-scroll-down" src="web/img/icon/icon-scroll-down.png"> 
+
+</section>
+<section class="classement-content-wrapper">
+
+		<div class="full-height content-border">
+			
+		</div>
+
+</section>
+
+
