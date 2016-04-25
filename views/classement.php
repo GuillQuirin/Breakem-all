b5cdde5c5cd3d543829ef2b2e2bce856d74b83b9
Teddy Meksavanh <teddy@mobilispro.com>
2016-04-25 22:40:57 +0200

    Classement relink

--- views/classement.php
+++ views/classement.php
@@ -18,19 +18,37 @@
 </section>
 <section class="classement-content-wrapper">
 
-	<div class="container content-border m-a">
+	<div class="container full-width">
 
 		<div class="row">
+			<div class="grid-md-8 classement-content-main">
 
-			<div class="grid-md-12" style="height:200px; border:1px solid #ffffff; margin:15px 0;">
+				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
+					<div class="classement-header-ranking-wrapper">
+						<img src="web/img/heroes-of.jpg" class="img-cover">
+					</div>
+				</div>
+				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
+					<div class="classement-header-ranking-wrapper">
+						<span class="classement-header-ranking">1</span>		
+					</div>
+				</div>			
+
 			</div>
-			<div class="grid-md-12" style="height:200px; border:1px solid #ffffff; margin:15px 0;">
-			</div>
-			<div class="grid-md-12" style="height:200px; border:1px solid #ffffff; margin:15px 0;">
-			</div>
-			<div class="grid-md-12" style="height:200px; border:1px solid #ffffff; margin:15px 0;">
-			</div>
-			<div class="grid-md-12" style="height:200px; border:1px solid #ffffff; margin:15px 0;">
+
+			<div class=" grid-md-4 classement-content-navbar-side">
+
+				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
+					<div class="classement-header-ranking-wrapper">
+						<span class="classement-header-ranking">1</span>		
+					</div>
+				</div>
+				<div class="grid-md-12" style="border:1px solid #ffffff; margin:15px 0;">
+					<div class="classement-header-ranking-wrapper">
+						<span class="classement-header-ranking">1</span>		
+					</div>
+				</div>	
+
 			</div>
 		</div>
 
