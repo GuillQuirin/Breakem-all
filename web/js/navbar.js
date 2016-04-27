84646dee225fbd264e4041568ab85aeb6d886374
Dylan <dylanfoster.esgi@gmail.com>
2016-04-27 19:14:54 +0200

    register premices

--- web/js/navbar.js
+++ web/js/navbar.js
@@ -42,11 +42,122 @@ window.addEventListener('load', function load(){
 	$(window).scroll(function(){
 		ShrinkNavbar();
 	});
+	register.init();
 	connection.init();
 	deconnection.init();
 });
 
 
+var register = {
+	init: function(){
+		this.setFormToWatch();
+		if(this.getFormToWatch() instanceof jQuery){
+			this.sendEvent();
+		};		
+	},
+	setFormToWatch: function(){this._form = jQuery("#connection-form");},
+	getFormToWatch: function(){return this._form;},
+	isEmailValid: function(jQEmail){
+		var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(jQEmail.val().match(mailformat) || jQEmail.val().length == 0){
+			return true;
+		}
+		this.highlightInput(jQEmail);
+		return false;
+	},
+	isPasswordValid: function(jQPassword){
+		var unauthorizedChars = /[^a-zA-Z-0-9]/;
+		if(jQPassword.val().match(unauthorizedChars) || jQPassword.val().length == 0){
+			this.highlightInput(jQPassword);
+			return false;
+		}
+		return true;
+	},
+	highlightInput: function(jQinput){
+		jQinput.addClass('failed-input');
+		jQinput.val('');
+		jQinput.focus();
+		this.removeFailAnimationEvent(jQinput);
+	},
+	tryParseServerData: function(rawData){
+		try {
+			var obj = jQuery.parseJSON(rawData);
+			return obj;
+		}
+		catch(err) {
+			console.log(rawData);
+			alert("Problem during server processes \n Check console for details");
+		}
+		return false;
+	},
+	treatParsedJson: function(obj){
+		if(obj.connected){
+			location.reload();
+		}
+		else{
+			this.highlightInput(this._email);
+			this.highlightInput(this._password);
+			if(obj.errors.inputs){
+				// missing input !
+				alert("you are missing an input");
+			}
+			else if(obj.errors.user){
+				// email and pass don't match
+				alert("password and email don't match");
+			}
+		}
+	},
+
+
+	/*### Send Form event ###*/
+	sendEvent: function(){
+		var _this = this;
+		var _form = this.getFormToWatch();
+		var _email = _form.find("input[name='email']");
+		var _password = _form.find("input[name='password']");
+
+		this._email = _email;
+		this._password = _password;
+
+		_form.submit(function(event) {
+			if (_this.isEmailValid(_email) && _this.isPasswordValid(_password)) {
+				jQuery.ajax({
+				  url: 'index/connection',
+				  type: 'POST',
+				  data: {
+				  	email: _email.val(),
+				  	password: _password.val()
+				  },
+				  complete: function(xhr, textStatus) {
+				    // console.log("request complted \n");
+				  },
+				  success: function(data, textStatus, xhr) {
+				    var obj = _this.tryParseServerData(data);
+				    if(obj != false){
+				    	_this.treatParsedJson(obj);
+				    }
+				  },
+				  error: function(xhr, textStatus, errorThrown) {
+				    console.log("request error !! : \t " + errorThrown);
+				  }
+				});
+				
+				// return true;
+			};
+			event.preventDefault();
+			return false;
+		});
+
+	},
+
+	/*### Remove Animation on keyup event ###*/
+	removeFailAnimationEvent: function(jQInput){
+		// Le one() permet de ne declencher l'event (keyup ici) qu'une seule fois puis de le supprimer automatiquement
+		jQInput.one('keyup', function() {
+			jQInput.removeClass('failed-input');
+		});
+	}
+}
 var connection = {
 	init: function(){
 		this.setFormToWatch();
