ed0e833764f21174f45207e9fcfc39985696fa11
Rémy Cariou <Rmy Cariou>
2016-05-03 22:20:46 +0200

    Mise à jour de ma branche (nettoyage)

--- core/routing.class.php
+++ core/routing.class.php
@@ -5,11 +5,9 @@ class routing{
 		$uri = $_SERVER['REQUEST_URI'];
 		$explode_uri = explode("?",$uri);
 		$uri = $explode_uri[0];
-<<<<<<< HEAD
 		$uri = trim(str_replace("esgi2/","",$uri),"/");
-=======
 		$uri = trim(str_replace(str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']),"",$uri),"/");
->>>>>>> 5ccafac37c19b5147e6d999a92a1f3bbd55409c0
+
 		$explode_uri = explode("/",$uri);
 		//print_r($explode_uri);
 		$c = (!empty($explode_uri[0]))?$explode_uri[0]:"index";
--- index.php
+++ index.php
@@ -28,13 +28,9 @@ $route = routing::setRouting();
 $name_page = $route["c"];
 $name_controller = $route["c"]."Controller";
 $path_controller = "controllers/".$name_controller.".class.php";
-<<<<<<< HEAD
 //var_dump($path_controller);
 // var_dump(['name_page' => $name_page], ['name_controller' => $name_controller], ['path_controller' => $path_controller]);
-=======
 
-
->>>>>>> 5ccafac37c19b5147e6d999a92a1f3bbd55409c0
 if(file_exists($path_controller)){
 	require_once($path_controller);
 	$controller = new $name_controller;
