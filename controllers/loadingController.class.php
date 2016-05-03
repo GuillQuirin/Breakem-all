50403002eec7d872fa431350fea7b0da19b714a6
unknown <ventialaw@hotmail.fr>
2016-05-03 15:18:07 +0200

    Page Loading

---
+++ controllers/loadingController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class loadingController{
+	public function __construct(){
+		$v = new View();
+
+		$v->assign("css", "loading");
+		$v->assign("js", "loading");
+		$v->assign("title", "En chargement");
+		$v->assign("content", "Page de Chargement");
+        $v->setView("loading");
+    }
+}
