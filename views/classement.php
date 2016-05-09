be922b775ead4ec76a304eb3f6e30e9a68df6f5a
Teddy Meksavanh <teddy@mobilispro.com>
2016-05-09 12:26:22 +0200

    Teddy Css saleeee

--- views/classement.php
+++ views/classement.php
@@ -9,34 +9,40 @@
 				<span>Découvrez les meilleurs joueurs et équipes de vos jeux préférés!</span>
 			</div>
 			<div class="">
-				<select type="text" class="select-default classement-select-default">
+				<select type="text" class="select-default classement-select-default classement-select-game">
+					<option>Jeux</option>
+					<option>SSBB</option>
+					<option>CS</option>					
+					<option>DOTA</option>
+				</select>
+				<select type="text" class="select-default classement-select-default">					
 					<option>Team</option>
 					<option>Solo</option>					
 					<option>Recherche avancé</option>
-				</select>
-			</div>			
+				</select>				
+			</div>				
 		</div>
 		<div class="classement-header-right animation fadeRight">
 			<div class="classement-select background-wrapper shadow-bottom-full">				
 				<?php echo '<img class="classement-select-image" src="' . WEBPATH . '/web/img/ehome.jpg">';?>
-				<span class="classement-select-title">EHOME</span>
-				<span class="classement-select-quote">"La victoire se fait grâce au travail d'équipe."</span>
-				<span class="classement-select-pts">8098 PTS</span>
-				<?php echo '<img class="classement-select-logo" src="' . WEBPATH . '/web/img/">';?>						
+				<span class="pad classement-select-title">EHOME</span>
+				<span class="pad classement-select-quote">"La victoire se fait grâce au travail d'équipe."</span>
+				<span class="pad classement-select-pts">8098 PTS</span>
+				<span class="classement-ranking-icon">1</span>										
 			</div>	
 			<div class="classement-select background-wrapper shadow-bottom-full">				
 				<?php echo '<img class="classement-select-image classement-select-image-2" src="' . WEBPATH . '/web/img/ehome.jpg">';?>
-				<span class="classement-select-title classement-select-title-2">EHOME</span>
-				<span class="classement-select-quote classement-select-quote-2">"Win to get the Bitches"</span>
-				<span class="classement-select-pts classement-select-pts-2">7999 PTS</span>	
-				<?php echo '<img class="classement-select-logo classement-select-logo-2" src="' . WEBPATH . '/web/img/">';?>		
+				<span class="pad classement-select-title classement-select-title-2">EHOME</span>
+				<span class="pad classement-select-quote classement-select-quote-2">"Win to get the Bitches"</span>
+				<span class="pad classement-select-pts classement-select-pts-2">7999 PTS</span>	
+				<span class="classement-ranking-icon ri-2">2</span>	
 			</div>	
 			<div class="classement-select background-wrapper shadow-bottom-full">				
 				<?php echo '<img class="classement-select-image classement-select-image-3" src="' . WEBPATH . '/web/img/ehome.jpg">';?>
-				<span class="classement-select-title classement-select-title-3">EHOME</span>
-				<span class="classement-select-quote classement-select-quote-3">"I'll find you and i'll kill you!"</span>
-				<span class="classement-select-pts classement-select-pts-3">6410 PTS</span>	
-				<?php echo '<img class="classement-select-logo classement-select-logo-3" src="' . WEBPATH . '/web/img/">';?>		
+				<span class="pad classement-select-title classement-select-title-3">EHOME</span>
+				<span class="pad classement-select-quote classement-select-quote-3">"I'll find you and i'll kill you!"</span>
+				<span class="pad classement-select-pts classement-select-pts-3">6410 PTS</span>	
+				<span class="classement-ranking-icon ri-3">3</span>		
 			</div>			
 		</div>
 	</div>
@@ -51,78 +57,21 @@
 		<div class="row classement-content-row">
 			<div class="grid-md-12">
 
-				<div class="classement-nav-hori-wrapper grid-md-12">
-					
-					<div class="nav_hori">
-						<ul>
-							<li class="border_menu active_menu menu_separ"><a href="#">Equipe</a></li>
-							<li class="border_menu menu_separ"><a href="#">Solo</a></li>
-							<li class="border_menu menu_separ"><a href="#">5vs5</a></li>
-							<li class="border_menu menu_separ"><a href="#">2vs2</a></li>
-							<li class="border_menu menu_separ"><a href="#">Plus...</a></li>
-						</ul>
-					</div>
-
-					<div class="classement-select-hori-wrapper">
-						<ul>
-							<li>						
-								<div>
-									<label class="classement-select-hori-label">Trier par Jeux :</label>
-									<select class="classement-select-hori select-default">
-										<option>Tout</option>
-										<option>Super Smash Bros Melee</option>
-										<option>Mario Kart</option>
-										<option>Heroes of the Storm</option>
-									</select>
-								</div>
-							</li>
-						</ul>
-					</div>
-				
-				</div>
-
-				<!-- classement -->
-				<div class="grid-md-6 classement-resultat-wrapper">
-					<div class="grid-md-6 grid-md-offset-3 classement-resultat">				
-						<img src="web/img/heroes-of.jpg" class="img-cover">	
-						<span class="classement-ranking-icon">1</span>	
-					</div>
-					<div class="grid-md-6">
-						<div class="classement-resultat">
-							<img src="web/img/heroes-of.jpg" class="img-cover">	
-							<span class="classement-ranking-icon">2</span>	
-						</div>	
-					</div>
-					<div class="grid-md-6">
-						<div class="classement-resultat">
-							<img src="web/img/heroes-of.jpg" class="img-cover">
-							<span class="classement-ranking-icon">3</span>		
-						</div>		
-					</div>
-				</div>
-				<!-- f classement -->
-
-				<!-- classement -->
-				<div class="grid-md-6 classement-resultat-wrapper">
-					<div class="grid-md-6 grid-md-offset-3 classement-resultat">				
-						<img src="web/img/heroes-of.jpg" class="img-cover">	
-						<span class="classement-ranking-icon">1</span>	
-					</div>
-					<div class="grid-md-6">
-						<div class="classement-resultat">
-							<img src="web/img/heroes-of.jpg" class="img-cover">	
-							<span class="classement-ranking-icon">2</span>	
-						</div>	
-					</div>
-					<div class="grid-md-6">
-						<div class="classement-resultat">
-							<img src="web/img/heroes-of.jpg" class="img-cover">
-							<span class="classement-ranking-icon">3</span>		
-						</div>		
-					</div>
-				</div>
-				<!-- f classement -->
-
+			<div class="classement-select background-wrapper shadow-bottom-full">				
+				<?php echo '<img class="classement-select-image classement-select-image-3" src="' . WEBPATH . '/web/img/ehome.jpg">';?>
+				<span class="pad classement-select-title classement-select-title-3">EHOME</span>
+				<span class="pad classement-select-quote classement-select-quote-3">"I'll find you and i'll kill you!"</span>
+				<span class="pad classement-select-pts classement-select-pts-3">6410 PTS</span>	
+				<span class="classement-ranking-icon ri-3">4</span>		
+			</div>
+			<div class="classement-select background-wrapper shadow-bottom-full">				
+				<?php echo '<img class="classement-select-image classement-select-image-3" src="' . WEBPATH . '/web/img/ehome.jpg">';?>
+				<span class="pad classement-select-title classement-select-title-3">EHOME</span>
+				<span class="pad classement-select-quote classement-select-quote-3">"I'll find you and i'll kill you!"</span>
+				<span class="pad classement-select-pts classement-select-pts-3">6410 PTS</span>	
+				<span class="classement-ranking-icon ri-3">5</span>		
+			</div>						
+						
 			</div>
 
 		
