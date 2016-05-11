ee10f2fad3d760fdbf3db2cf4a52fe18629cb99e
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-05-11 13:08:48 +0200

    Requetes SQL pour récupérer la liste des données dans /Admin

--- models/adminManager.class.php
+++ models/adminManager.class.php
@@ -5,10 +5,56 @@ class adminManager extends basesql{
 		parent::__construct();
 	}
 
+	//Membre
 	public function getListUser(){
-		$sql = "SELECT id, name, firstname, pseudo, birthday, description, kind, city, email, status, img, idTeam, isConnected, lastConnexion FROM User ";
-		$query = $this->pdo->query($sql)->fetchAll();
+		$sql = "SELECT id, name, firstname, pseudo, birthday, description, kind, city, email, status, img, idTeam, isConnected, lastConnexion FROM User ORDER BY pseudo ASC";
 		
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute();
+		$query = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+		if($query === FALSE)
+			return false;
+
+		return $query;
+	}
+
+	//Tournoi
+	public function getListTournament(){
+		$sql = "SELECT id, name, firstname, pseudo, birthday, description, kind, city, email, status, img, idTeam, isConnected, lastConnexion FROM User ORDER BY pseudo ASC";
+		
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute();
+		$query = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+		if($query === FALSE)
+			return false;
+
+		return $query;
+	}
+	
+	//Team
+	public function getListTeam(){
+		$sql = "SELECT id, name, firstname, pseudo, birthday, description, kind, city, email, status, img, idTeam, isConnected, lastConnexion FROM User ORDER BY pseudo ASC";
+		
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute();
+		$query = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+		if($query === FALSE)
+			return false;
+
+		return $query;
+	}
+
+	//Commentaire
+	public function getListComs(){
+		$sql = "SELECT id, name, firstname, pseudo, birthday, description, kind, city, email, status, img, idTeam, isConnected, lastConnexion FROM User ORDER BY pseudo ASC";
+		
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute();
+		$query = $sth->fetchAll(PDO::FETCH_ASSOC);
+
 		if($query === FALSE)
 			return false;
 
