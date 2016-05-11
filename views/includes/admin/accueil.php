9eaa715777a8aab3bd8000e7a61aa843019ec3d9
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-05-11 13:41:46 +0200

    MAJ de l'affichage infos user dans /admin

--- views/includes/admin/accueil.php
+++ views/includes/admin/accueil.php
@@ -7,12 +7,13 @@
 					echo "<table class='admin-table-member' border='1'>";
 					foreach ($listejoueur as $ligne => $joueur) {
 						echo "<tr>";
-							echo "<td>".$joueur->getPseudo()."</td>";
+							echo "<td><a href='".WEBPATH."/profil?pseudo=".$joueur->getPseudo()."'>".$joueur->getPseudo()."<a/></td>";
 							echo "<td>".$joueur->getEmail()."</td>";
 							echo "<td>".$joueur->getStatus()."</td>";
-							echo "<td>".$joueur->getImg()."</td>";
+							echo "<td><img src='".$joueur->getImg()."'></td>";
 							echo "<td>".$joueur->getIdTeam()."</td>";
 							echo "<td>".$joueur->getIsConnected()."</td>";
+							echo "<td><button>Desactiver</button></td>";
 						echo "</tr>";
 					}
 					echo "</table>";
