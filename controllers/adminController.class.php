fc82c17c53c5f32de38187133b132e4cd2708dc5
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-05-12 10:37:52 +0200

    Redirection vers 404 si un non-admin tente d'accèder au back

--- controllers/adminController.class.php
+++ controllers/adminController.class.php
@@ -1,27 +1,31 @@
 <?php
 class adminController extends template{
     public function adminAction(){
-        $v = new view();
-		$this->assignConnectedProperties($v);
-        $v->assign("css", "admin");
-        $v->assign("js", "admin");
-        $v->assign("title", "admin");
-        $v->assign("content", "Liste des Utilisateurs");
+        if(isset($_isAdmin) && $_isAdmin == 1){
+            $v = new view();
+    		$this->assignConnectedProperties($v);
+            $v->assign("css", "admin");
+            $v->assign("js", "admin");
+            $v->assign("title", "admin");
+            $v->assign("content", "Liste des Utilisateurs");
 
-        $admin = new adminManager();
+            $admin = new adminManager();
 
-		$listejoueurs = $admin->getListUser();  
+    		$listejoueurs = $admin->getListUser();  
 
-        $listeplatforms = $admin->getListPlatform();
-        
-        $listesignalement = $admin->getListReports();
+            $listeplatforms = $admin->getListPlatform();
+            
+            $listesignalement = $admin->getListReports();
 
-        $v->assign("listejoueur",$listejoueurs);
+            $v->assign("listejoueur",$listejoueurs);
 
-        $v->assign("listeplatform",$listeplatforms);
+            $v->assign("listeplatform",$listeplatforms);
 
-        $v->assign("listesignalement",$listesignalement);
-
-        $v->setView("/includes/admin/accueil", "template");
+            $v->assign("listesignalement",$listesignalement);
+           
+            $v->setView("/includes/admin/accueil", "template");
+        }
+        else //On affiche la 404 pour faire croire que le mec tape n'importe quoi
+            header('Location: '.WEBPATH.'/404');
     }
 }
