d12fc7013c04c0a3aea8447b808296e96eed12be
unknown <ventialaw@hotmail.fr>
2016-05-13 00:59:08 +0200

    ajax this

--- web/js/admin.js
+++ web/js/admin.js
@@ -59,38 +59,37 @@ var platformModule = {
 						"<div class='grid-md-4'><div class='admin-data-ihm-elem'><span>" + field.description + "</span></div></div>" +
 						"<div class='grid-md-4'><div class='admin-data-ihm-elem'><div class='admin-data-ihm-elem-img-wrapper'><img class='img-cover' src='" + field.img + "'></div></div></div>" +
 
-						"<div class='index-modal hidden'>" +
+						"<div class='admin-data-ihm-btn hidden relative'>" +
+							"<button class='admin-btn-default admin-btn-modify' type='button'><span>Modifier</span></button>" +  
+							"<button class='admin-btn-default admin-btn-delete' type='button'><span>Supprimer</span></button>" + 
+
+							"<div class='index-modal platforms hidden'>" +
 
 							"<div class='index-modal-login align form-bg-active'>" +
 								
-								"<div id='login-form' class='grid-md-3 inscription_rapide animation fade'>" +
-									"<form id='login-form'>" +			    
-									    "<label for='email'>Nom :</label>" +
-									    "<input class='input-default admin-form-input-w' id='nom' name='nom' type='text' placeholder='" + field.name + "'>" +
-									     "<label for='email'>Description :</label>" +
-									    "<textarea class='input-default admin-form-input-w' id='description' name='description' type='text' placeholder='" + field.description + "'></textarea>" +							    							  
-									    "<div class='admin-avatar-wrapper m-a'>" +																	
-											"<img class='admin-avatar img-cover' src='" + field.img + "' title='Image de profil' alt='Image de profil'>" +										
-										"</div>" +	
-										"<div class='text-center admin-input-file'>" +								 
-										"<input type='file' name='profilpic'>" +
-										"</div>" +
-									    "<button type='button' class='btn btn-pink'><a>Valider</a></button>" +
-							  		"</form>" +
-							  	"</div>" + 	 
+									"<div id='login-form' class='grid-md-3 inscription_rapide animation fade'>" +
+										"<form id='login-form'>" +			    
+										    "<label for='email'>Nom :</label>" +
+										    "<input class='input-default admin-form-input-w' id='nom' name='nom' type='text' placeholder='" + field.name + "'>" +
+										     "<label for='email'>Description :</label>" +
+										    "<textarea class='input-default admin-form-input-w' id='description' name='description' type='text' placeholder='" + field.description + "'></textarea>" +							    							  
+										    "<div class='admin-avatar-wrapper m-a'>" +																	
+												"<img class='admin-avatar img-cover' src='" + field.img + "' title='Image de profil' alt='Image de profil'>" +										
+											"</div>" +	
+											"<div class='text-center admin-input-file'>" +								 
+											"<input type='file' name='profilpic'>" +
+											"</div>" +
+										    "<button type='button' class='btn btn-pink'><a>Valider</a></button>" +
+								  		"</form>" +
+								  	"</div>" + 	 
+								"</div>" +
 							"</div>" +
 						"</div>" +
-
-						"<div class='admin-data-ihm-btn hidden'>" +
-							"<button class='admin-btn-default admin-btn-modify' type='button'><span>Modifier</span></button>" +  
-							"<button class='admin-btn-default admin-btn-delete' type='button'><span>Supprimer</span></button>" + 
-						"</div>" +
-
 					"<div>" 
 				);
 			});
 			platformModule.ihmElemHover();
-			platformModule.openForm(".admin-btn-modify");
+			platformModule.openForm(".admin-btn-modify", ".platforms");
 		});
 	},
 	ihmElemHover : function(){
@@ -102,9 +101,10 @@ var platformModule = {
 		  }
 		);
 	},
-	openForm : function(selector){
+	openForm : function(selector, myForm){
 		jQuery(selector).click(function(){
-			jQuery(this).parent().find('.index-modal').removeClass('hidden');
+			jQuery('.index-modal').addClass('hidden');
+			jQuery(myForm).removeClass('hidden');
 		});
 	}
 };
