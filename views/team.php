45982ab6e9d44abbee6e0bdef578d34a0596de5f
Meghan Wlo <meghan.wlodarczyk@outlook.com>
2016-05-16 12:15:00 +0200

    Modification des liens Team = Liste des teams detailteam = UNE team en particulier

--- views/team.php
+++ views/team.php
@@ -1,59 +1,11 @@
-<section class="middle-height bg-cover-classement relative  align full-height">
+<h2 style="margin-top:100px"> Voici la liste des teams :</h2>
 
-    <div id="ID" class=" ">
-        <img id="profil" class="grid-md-5 align full-height" src="<?php echo $img; ?>">
-        <span class="header-title align full-height" id="name">Rayz</span>
-    </div>
-</section>
+<div style="margin-top:20px"class="inscription_team">
+    Nom de la team 1: <?php echo $name; ?><br>
+    Slogan : <br>
+    Description : <br>
 
-<section class="classement-content-wrapper  align full-height" id ="onglet">
-
-    <div class="container m-a content-border " id="container">
-        <div id="container1">
-            <div class="activite ">
-
-                <div class="border"><img class="img" src="web/img/logo-esgi.png"><div><p class="team">ESGI</br></br></br>Il y a 3 mois</br></br></br>ESGI team ssb, mk</p></div></div>
-                <div class="border"><img class="img" src="web/img/logo-esgi.png"><div><p class="team">ESGI</br></br></br>Il y a 3 mois</br></br></br>ESGI team ssb, mk</p></div></div>
-                <div class="border"><img class="img" src="web/img/logo-esgi.png"><div><p class="team">ESGI</br></br></br>Il y a 3 mois</br></br></br>ESGI team ssb, mk</p></div></div>
-            </div>
-        </div>
-        <div id="container2">
-            <div id="member">
-                <p id="member1">Membres</p>
-            </div>
-            <div id="member2" >
-                <img id="profil1" class="grid-md-5 " src="web/img/Rayz.jpg">
-                <span class=" name" >Rayz</span>
-                <img id="profil1" class="grid-md-5 " src="web/img/Rayz.jpg">
-                <span class=" name" >Rayz</span>
-                <img id="profil1" class="grid-md-5 " src="web/img/Rayz.jpg">
-                <span class="name"  >Rayz</span>
-            </div>
-        </div>
-        <div id="container3">
-            <div id="match">
-                <p id="match1">Prochain matchs</p>
-            </div>
-            <div id="match2">
-                <p id ="match3">Navi VS Fnatic</p>
-            </div>
-        </div>
-
-    </div>
-
-</section>
-
-<!--<form id="teamcreation-form">
-	<input type="text" name="name" placeholder="nom">
-	<input type="text" name="slogan" placeholder="slogan">
-	<textarea name="description" placeholder="description"></textarea>
-	<button type="button" class="btn btn-blue"><a>valider</a></button>
-</form>-->
-
-<footer>
-</footer>
-
-<script src="jquery.js"></script>
-
-</body>
-</html>
+    <button id='validate-form-team' type='submit' class='btn btn-pink'>
+        <a>Voir la team</a>
+    </button>       
+</div>
