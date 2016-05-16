484d8b8be4eaa0123355a714c3240762f12847ca
dydyFos <YOUR EMAIL ADRESS>
2016-05-17 00:00:46 +0200

    suppression de create et construct (identiques au parents) dans teamManager.class.php

--- models/teamManager.class.php
+++ models/teamManager.class.php
@@ -1,61 +1,19 @@
 <?php 
 
 class teamManager extends basesql{
-	public function __construct(){
-		/*
-			L'appel du constructeur de la classe-mère basesql va permettre
-			de cibler uniquement la table correspondant 
-			au nom du manager : ici sera la table Team
-		*/
-		parent::__construct();
-	}
-
-	
-	public function create(team $team){	
-	// Check afin de savoir qui appele cette méthode
-		$e = new Exception();
-		$trace = $e->getTrace();
-
-		// get calling class:
-		$calling_class = (isset($trace[1]['class'])) ? $trace[1]['class'] : false;
-		// get calling method
-		$calling_method = (isset($trace[1]['function'])) ? $trace[1]['function'] : false;
-
-
-		if(!$calling_class || !$calling_method)
-			header('Location: '.WEBPATH);
-
-		// Si appelée depuis la page tournoi
-		if ($calling_class === "creationtournoiController" 
-				&& $calling_method === "finalValidationAction"){
-
-			$this->columns = [];
-			$team_methods = get_class_methods($team);
-
-			foreach ($team_methods as $key => $method) {
-				if(strpos($method, 'get') !== FALSE){
-					$col = lcfirst(str_replace('get', '', $method));
-					$this->columns[$col] = $team->$method();
-				};
-			}
-			// Toutes les propriétés à 0 sont remove de l'array à ce moment là
-			// Pas impactant ici puisque les default value dans tournoi sont à 0
-			$this->columns = array_filter($this->columns);
-
-			$this->save();
-
-		}
-		else
-			header('Location: '.WEBPATH);		
-	}
 	
 	/*AJOUT PRESIDENT TEAM*/
-	public function setOwnerTeam(team $t, $idUser){
+	public function setOwnerTeam(team $t, user $u){
 		$sql = "INSERT INTO rightsteam (id, idUser, idTeam, right) 
-				VALUES ('', '".$idUser."', '".$idTeam."', '1')";
-		$query = $this->pdo->query($sql);
-		
-		return $query;
+				VALUES ('', ':idUser', ':idTeam', '1')";
+		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+		$sth->execute([
+			':idUser' => $u->getId(),
+			':idTeam' => $t->getId()
+		]);
+		$r = $sth->fetchAll();
+
+		return (bool) $r[0][0];
 	}
 
 	/*VERIFICATION DE L'UNICITE DU NOM TEAM*/
@@ -84,7 +42,9 @@ class teamManager extends basesql{
 	}
 
 	/*RECUPERATION TEAM*/
+	/* utilisez les miroirs svpppp getTeam(team $t)*/
 	public function getTeam(array $infos){
+
 		//tab[name]='Test'
 		$cols = array_keys($infos);
 		$data = [];
