e9753145b8a0f114c09b6ffd52a00dcc8a8725e7
Meghan Wlo <meghan.wlodarczyk@outlook.com>
2016-05-17 21:51:48 +0200

    Petite modif team

--- views/team.php
+++ views/team.php
@@ -1,6 +1,6 @@
 <h2 style="margin-top:100px"> Voici la liste des teams :</h2>
 
-<div style="margin-top:20px"class="inscription_team">
+<div style="margin-top:20px" class="inscription_team">
     Nom de la team 1: <?php echo $name; ?><br>
     Slogan : <br>
     Description : <br>
