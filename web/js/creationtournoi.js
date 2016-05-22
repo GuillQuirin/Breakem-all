06742b821347bc7f731f0970cde57e09349ee7e5
dydyFos <YOUR EMAIL ADRESS>
2016-05-22 20:48:31 +0200

    alerte lorsqu'on tente de quitter la creation de tournoi

--- web/js/creationtournoi.js
+++ web/js/creationtournoi.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function load(){
 	// Cette ligne permet la 'supression' de l'event de load pour liberer du cache (on devrait faire ça idéalement pour tous les events utilisés une seule fois) 
 	window.removeEventListener('load', load, false);
-	// preventQuitPageEvent();
+	preventQuitPageEvent();
 	if(dom.init())
 		gameTypesChoice.init();
 	navbar.preventShrink = true;
