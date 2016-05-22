07eee273a02aab75ed26eae8a860cd2339fb0c60
dydyFos <YOUR EMAIL ADRESS>
2016-05-22 21:48:22 +0200

    margin js adaptation to navbar height

--- web/js/detailtournoi.js
+++ web/js/detailtournoi.js
@@ -2,6 +2,19 @@ window.addEventListener('load', function load(){
 	// Cette ligne permet la 'supression' de l'event de load pour liberer du cache (on devrait faire ça idéalement pour tous les events utilisés une seule fois) 
 	window.removeEventListener('load', load, false);
 	// preventQuitPageEvent();
-	
+	dom.init();
 });
 
+var dom = {
+	init: function(){
+		this.setDetailTournoiInfos();
+		if(!!this.getDetailTournoiInfos())
+			adaptMarginToNavHeight(this.getDetailTournoiInfos());
+	},
+	setDetailTournoiInfos: function(){
+		this._detailTournoiInfos = $('section.detailtournoi-infos');
+	},
+	getDetailTournoiInfos: function(){
+		return this._detailTournoiInfos;
+	}
+}
