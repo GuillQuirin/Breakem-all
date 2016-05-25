9b567b9a48be798897d494b0596a7bb04a8b789c
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-05-25 15:37:38 +0200

    Intégration et organisation du CRUD de gametype dans le controlleur

--- controllers/adminController.class.php
+++ controllers/adminController.class.php
@@ -8,6 +8,7 @@ class adminController extends template{
             $v->assign("css", "admin");
                 $js['admin']="admin";
                 $js['platforms']="platforms";
+                $js['gametype']="gametype";
             $v->assign("js",$js);                                       
             $v->assign("title", "admin");
             $v->assign("content", "Liste des Utilisateurs");
@@ -44,6 +45,8 @@ class adminController extends template{
             header('Location: '.WEBPATH.'/404');
     }
 
+    /* PLATEFORME */
+
     public function getPlatformsDataAction(){
         $pm = new platformManager();
         $typesObj = $pm->getListPlatform();
@@ -64,6 +67,7 @@ class adminController extends template{
         
     }
 
+    /* TEAM */
     public function updateTeamStatusAction(){
          if(!empty($_POST['checkbox_team'])){
             $filteredinputs = array_filter(filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING));
@@ -88,6 +92,7 @@ class adminController extends template{
        header('Location: '.WEBPATH.'/admin');
     }
 
+    /* USER */
     public function updateUserStatusAction(){
         $args = array(
             'pseudo' => FILTER_SANITIZE_STRING,
@@ -104,6 +109,7 @@ class adminController extends template{
         $userBDD->setUser($user, $newuser);
     }
 
+    /* SIGNALEMENT */
     public function DeleteReportsAction(){
         $args = array(
             'id' => FILTER_VALIDATE_INT
@@ -120,19 +126,43 @@ class adminController extends template{
 
     }
 
-    public function DeleteTypeGameAction(){
+    /* TYPE GAME */
+
+     public function createTypeGameByAction(){
         $args = array(
-            'id' => FILTER_VALIDATE_INT
+            //'id' => FILTER_VALIDATE_INT,
+            'name' => FILTER_SANITIZE_STRING,
+            'description' => FILTER_SANITIZE_STRING,
+            'profilpic' => FILTER_VALIDATE_INT
         );
         
         $filteredinputs = filter_input_array(INPUT_POST, $args);
         
+        if(isset($_FILES['profilpic'])){
+
+            $uploaddir = '/web/img/';
+            $uploadfile = getcwd().$uploaddir.$this->getConnectedUser()->getId().'.jpg';
+
+            define('KB', 1024);
+            define('MB', 1048576);
+            define('GB', 1073741824);
+            define('TB', 1099511627776);
+
+            if($_FILES['profilpic']['size'] < 1*MB){
+                if($_FILES['profilpic']['error']==0){
+                    if(!move_uploaded_file($_FILES['profilpic']['tmp_name'], $uploadfile))
+                       die("Erreur d'upload");
+                }
+            }
+            $filteredinputs['img'] = $this->getConnectedUser()->getName().'.jpg';
+        }
+        var_dump($filteredinputs);
         $typegameBDD = new typegameManager();
-        $typegame = $typegameBDD->getTypeGame($filteredinputs['id']);
+        $typegameBDD->mirrorObject = new typegame($filteredinputs);
+        if($typegameBDD->create())
+            echo "CREATION";
+    }
 
-        $typegameBDD->delTypeGame($typegame);
-
-    } 
 
     public function getTypeGameByAction(){
         $args = array(
@@ -146,9 +176,61 @@ class adminController extends template{
 
         $arr = [];
         $arr['name'] = $obj->getName();
+        $arr['id'] = $obj->getId();
         $arr['img'] = $obj->getImg();
         $arr['description'] = $obj->getDescription();
 
         echo json_encode($arr);
     }
+
+    public function updateTypeGameByAction(){
+        $args = array(
+            'id' => FILTER_VALIDATE_INT,
+            'name' => FILTER_SANITIZE_STRING,
+            'description' => FILTER_SANITIZE_STRING,
+            'profilpic' => FILTER_VALIDATE_INT
+        );
+        
+        $filteredinputs = filter_input_array(INPUT_POST, $args);
+        
+        if(isset($_FILES['profilpic'])){
+
+            $uploaddir = '/web/img/';
+            $uploadfile = getcwd().$uploaddir.$this->getConnectedUser()->getId().'.jpg';
+
+            define('KB', 1024);
+            define('MB', 1048576);
+            define('GB', 1073741824);
+            define('TB', 1099511627776);
+
+            if($_FILES['profilpic']['size'] < 1*MB){
+                if($_FILES['profilpic']['error']==0){
+                    if(!move_uploaded_file($_FILES['profilpic']['tmp_name'], $uploadfile))
+                       die("Erreur d'upload");
+                }
+            }
+            $filteredinputs['img'] = $this->getConnectedUser()->getName().'.jpg';
+        }
+
+        $typegameBDD = new typegameManager();
+        $typegame = $typegameBDD->getTypeGame($filteredinputs['id']);
+        $typegameMAJ = new typegame($filteredinputs);
+        var_dump($typegame, $typegameMAJ);
+        if($typegameBDD->setTypeGame($typegame, $typegameMAJ))
+            echo "OK";
+    } 
+
+    public function DeleteTypeGameAction(){
+        $args = array(
+            'id' => FILTER_VALIDATE_INT
+        );
+        
+        $filteredinputs = filter_input_array(INPUT_POST, $args);
+        
+        $typegameBDD = new typegameManager();
+        $typegame = $typegameBDD->getTypeGame($filteredinputs['id']);
+
+        $typegameBDD->delTypeGame($typegame);
+
+    } 
 }
