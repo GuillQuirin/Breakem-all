088fc518f3d2329e2c96f2d5acf5d7bf6d0c996c
dydyFos <YOUR EMAIL ADRESS>
2016-06-04 15:34:23 +0200

    sous menus de l'arbre en dur (html)à fin de tests

--- views/creationtournoiDOM.php
+++ views/creationtournoiDOM.php
@@ -11,21 +11,40 @@
 	<img class="icon icon-size-3 down-center header-scroll-down" id="classement-header-scroll-down" src="web/img/icon/icon-scrollDown.png"> 
 </section>
 <section class="creationtournoi-tree-section display-flex-column full-width">
-	<div class="creationtournoi-tree-container display-flex-row m-a">
-		<div class="creationtournoi-tree-el display-flex-column m-a relative">
-			<p class="m-a text-center title title-4 lowercase">Type-de-jeu</p>
-		</div>
-		<div class="creationtournoi-tree-el display-flex-column m-a relative">
-			<p class="m-a text-center title title-4 lowercase">Jeu</p>
-		</div>
-		<div class="creationtournoi-tree-el display-flex-column m-a relative">
-			<p class="m-a text-center title title-4 lowercase">Console</p>
-		</div>
-		<div class="creationtournoi-tree-el display-flex-column m-a relative">
-			<p class="m-a text-center title title-4 lowercase">Règles</p>
-		</div>
-		<div class="creationtournoi-tree-el display-flex-column m-a relative">
-			<p class="m-a text-center title title-4 lowercase">Confirmation</p>
+	<div class="creationtournoi-tree-container full-width">
+		<div class="creationtournoi-tree-maxcontainer display-flex-row m-a full-width">
+			<div id="creationtournoi-tree-gametype" class="creationtournoi-tree-el display-flex-column m-a relative">
+				<p class="m-a text-center title title-4 lowercase">Type-de-jeu</p>
+				<div class="creationtournoi-tree-el-choices-container absolute display-flex-column full-width">
+					<div class="full-width tree-el-choice">
+						<p class="m-a text-center">objet 1</p>
+					</div>
+					<div class="full-width tree-el-choice">
+						<p class="m-a text-center">objet 2</p>
+					</div>
+					<div class="full-width tree-el-choice">
+						<p class="m-a text-center">objet 3</p>
+					</div>
+					<div class="full-width tree-el-choice">
+						<p class="m-a text-center">objet 4</p>
+					</div>
+					<div class="full-width tree-el-choice">
+						<p class="m-a text-center">objet 5</p>
+					</div>
+				</div>
+			</div>
+			<div id="creationtournoi-tree-game" class="creationtournoi-tree-el display-flex-column m-a relative">
+				<p class="m-a text-center title title-4 lowercase">Jeu</p>
+			</div>
+			<div id="creationtournoi-tree-platform" class="creationtournoi-tree-el display-flex-column m-a relative">
+				<p class="m-a text-center title title-4 lowercase">Console</p>
+			</div>
+			<div id="creationtournoi-tree-rules" class="creationtournoi-tree-el display-flex-column m-a relative">
+				<p class="m-a text-center title title-4 lowercase">Règles</p>
+			</div>
+			<div id="creationtournoi-tree-confirm" class="creationtournoi-tree-el display-flex-column m-a relative">
+				<p class="m-a text-center title title-4 lowercase">Confirmation</p>
+			</div>
 		</div>
 	</div>
 </section>
