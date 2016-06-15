a4bf73b0e7a7a691054f0f47e501535372eaf06d
Rémy Cariou <Rmy Cariou>
2016-06-15 11:03:49 +0200

    Ajout de la vue création team

---
+++ controllers/creationteamController.class.php
@@ -0,0 +1,67 @@
+<?php
+class creationTeamController extends template{
+    public function creationteamAction(){
+
+        //Initialisation de la vue
+        $v = new view();
+        $this->assignConnectedProperties($v);
+        $v->assign("css", "creationteam");
+        $v->assign("js", "creationteam");
+        $v->assign("title", "creationteam");
+        $v->assign("content", "Liste des teams");
+
+
+        //Liste des teams
+        $obj = new teamManager();
+        $v->setView("creationteam");
+    }
+
+
+
+public function addTeamAction()
+{
+
+    $args = array(
+//'id' => FILTER_VALIDATE_INT,
+        'name' => FILTER_SANITIZE_STRING,
+        'description' => FILTER_SANITIZE_STRING,
+        'img' => FILTER_SANITIZE_STRING,
+        'slogan' => FILTER_SANITIZE_STRING
+
+    );
+
+    $filteredinputs = filter_input_array(INPUT_POST, $args);
+
+    if (isset($_FILES['img'])) {
+
+        $uploaddir = '/web/img/';
+        $name = $_FILES['img']['name'];
+
+        $uploadfile = getcwd().$uploaddir.$name;
+//var_dump($uploadfile);
+
+        define('KB', 1024);
+        define('MB', 1048576);
+        define('GB', 1073741824);
+        define('TB', 1099511627776);
+
+        if ($_FILES['img']['size'] < 1 * MB) {
+            if ($_FILES['img']['error'] == 0) {
+
+                if (!move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile))
+                    die("Erreur d'upload");
+            }
+        }
+        $filteredinputs['img'] = $name;
+    }
+
+    $teamBDD = new teamManager();
+    $teamBDD->mirrorObject = new team($filteredinputs);
+    if ($teamBDD->create())
+        echo "CREATION";
+//var_dump($filteredinputs);
+
+}
+
+
+}
---
+++ views/creationteam.php
@@ -0,0 +1,27 @@
+<form action="admin/addGame" method="post" enctype="multipart/form-data">
+    <table border=0>
+        <tr>
+            <td>Nom</td>
+            <td><input type=text name="name"></td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td><textarea rows="3" name="description"></textarea></td>
+        </tr>
+        <tr>
+            <td>Image</td>
+            <td><input type=file name="img"></td>
+        </tr>
+        <tr>
+            <td>Slogan</td>
+            <td><input type="text" name="slogan"></td>
+        </tr>
+        <tr>
+            <td colspan=2>
+                <button id='validate-form-games' type='submit' class='btn btn-pink admin-form-submit'>
+                    <a>Créer ma team</a>
+                </button>
+            </td>
+        </tr>
+    </table>
+</form>
