a7ac63a8b9c22a0db2fd15e613602dc92f73d08d
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-07-03 18:20:12 +0200

    Mise en forme des CGU + retrait des href videspour cette page

--- views/CGU.php
+++ views/CGU.php
@@ -1,17 +1,17 @@
-<section class=" middle-height configuration-content-wrapper my-content-wrapper">
+<section class="paddingHeader configuration-content-wrapper my-content-wrapper">
 
 	<div class="container m-a content-border classement-container" style="border:none;">
 	
 	<section>
 		<article>
-		Article 1 : Objet
-		Les présentes conditions générales d’utilisation ont pour objet l’accès, l’inscription, la consultation et l’utilisation du site www.jeuxvideo.com, édité par la société Webedia (RCS Nanterre 501 106 520). Elles constituent un contrat entre l’utilisateur du site et des services proposés et la société Webedia.
+		<p>Article 1 : Objet</p>
+		Les présentes conditions générales d’utilisation ont pour objet l’accès, l’inscription, la consultation et l’utilisation du site www.breakem-all.com.
 		</article>
 		<article>
-		Article 2 : Acceptation des Conditions Générales
+		<p>Article 2 : Acceptation des Conditions Générales</p>
 		<ul>
 			<li>
-			2.1 En accédant, utilisant, consultant ou s’inscrivant sur les sites ou services proposés sur www.jeuxvideo.com, l’utilisateur s’engage à accepter sans réserve les présentes conditions générales. La consultation du site www.jeuxvideo.com ne nécessite pas d’inscription préalable.
+			2.1 En accédant, utilisant, consultant ou s’inscrivant sur les sites ou services proposés sur www.breakem-all.com, l’utilisateur s’engage à accepter sans réserve les présentes conditions générales.
 			</li>
 			<li>
 			2.2 Certains services nécessitent une inscription obligatoire, matérialisée par un formulaire d’inscription et des conditions générales spécifiques. Ces dernières doivent également être acceptées par l’utilisateur afin qu’il puisse bénéficier des services concernés.
@@ -19,115 +19,120 @@
 		</ul>
 		</article>
 		<article>
-		Article 3 : Vie privée et cookies
+		<p>Article 3 : Vie privée et cookies</p>
 		<ul>
 			<li>
-			3.1 Lors de son inscription sur le site, l’utilisateur accepte que soient collectées et conservées les données ci-après : Nom, prénom, email, sexe, code postal, date de naissance, ville pays, adresse IP.
-
-			D’autres données facultatives telles que l’adresse postale ou le numéro de téléphone peuvent également être communiquées par l’utilisateur s’il le souhaite.
+			3.1 Lors de son inscription sur le site, l’utilisateur accepte que soient collectées et conservées les données ci-après : email et date de naissance.
 
 			Les données d’inscription sont utilisées :
 
-			i) pour la participation aux différents services proposés sur le site (Forums, blogs, Quizz etc)
-			ii) pour la souscription par l’utilisateur à des services spécifiques (newsletter, concours),
-			iii) pour le fonctionnement technique et administratif du site.
-			Les données fournies sont conservées par la société Webedia pour la durée d’utilisation de l’utilisateur du site et des services proposés.
+			i) pour la participation aux différents services proposés sur le site 
+			ii) pour le fonctionnement technique et administratif du site.
 
 			Ces données sont conservées, en France, dans un environnement technique sécurisé.		
 			</li>
 			<li>
 			3.2 L’utilisateur peut à tout moment modifier ou supprimer les données personnelles fournies à travers son espace utilisateur (rubrique «Mon compte ») et ainsi exercer son droit d’accès.
 
-			La suppression définitive d’un compte utilisateur (matérialisé par le couple pseudo/IP uniquement) ne peut intervenir qu’après vérification de l’authenticité de la demande et épuisement des obligations relatives à la recherche d’infractions. Pour en savoir plus sur la suppression de compte, nous vous invitons à consulter notre FAQ : (http://www.jeuxvideo.com/forums_faq.htm).
+			La suppression définitive d’un compte utilisateur ne peut intervenir qu’après l'envoi d'un message aux administrateurs dans la rubrique "Nous contacter".
 
 			Il est rappelé à l’utilisateur que le pseudonyme à lui seul ne constitue pas une donnée personnelle.
 			</li>
 			<li>
-			3.3 La société Webedia dispose d’un correspondant Informatique et Libertés (CIL) dédié au site jeuxvideo.com. Ce dernier a procédé à l’inscription des traitements associés aux données collectées au sein d’un registre dédié.
-
-			L’utilisateur peut également exercer son droit d’accès aux données personnelles en s’adressant au CIL à travers l’adresse privacy@jeuxvideo.com et/ou en écrivant à Webedia - Jeuxvideo.com, Service Correspondant Informatique et Libertés, 2, Rue Paul Vaillant Couturier, CS60102, 92532 Levallois-Perret Cedex. La consultation du registre des traitements s’effectue auprès du CIL selon les mêmes modalités.
-			</li>
-			<li>
-			3.4.a Le site www.jeuxvideo.com utilise un système de cookies pour son fonctionnement. Ces cookies pourront contenir des données relatives à l’identification et à la navigation de l’utilisateur sur le site et les services proposés.
-
-			3.4.b Le site www.jeuxvideo.com utilise également un système de cookies et pixels publicitaires issus de partenaires tiers. Ce système de cookies et pixels permet la diffusion de publicités sélectionnées selon les préférences et affinités de l’utilisateur. Les données utilisées par ce système sont anonymes.
+			3.4.a Le site www.breakem-all.com utilise un système de cookies pour son fonctionnement. Ces cookies pourront contenir des données relatives à l’identification et à la navigation de l’utilisateur sur le site et les services proposés.
 
 			L’utilisateur dispose de la faculté de bloquer ou supprimer les cookies par le biais des options présentées au sein de son navigateur (Pour « Google Chrome » : Ctrl + Maj + Suppr ; pour « Internet Explorer » : Alt+X puis sélectionner le menu Option Internet ; pour « Mozilla firefox » : Onglet Firefox puis sélectionner le menu Options).
 
 			Note : Le blocage des cookies peut altérer le fonctionnement des services proposés sur Internet.
-
-			L’utilisateur peut également paramétrer ou désactiver les cookies publicitaires en se connectant à l’adresse : http://www.youronlinechoices.com/fr/.
 			</li>
 			<li>
-			3.5 Il appartient à l’utilisateur de s’assurer de la confidentialité du ou des mots de passe utilisés sur le site www.jeuxvideo.com. La société Webedia ne dispose d’aucun mot de passe et ne saurait être tenue pour responsable en cas de perte ou d’utilisation non conforme de cet élément. L’utilisateur est invité à signaler toute utilisation non autorisée de son espace utilisateur.
-			</li>
-			<li>
-			3.6 La société Webedia se réserve le droit de communiquer aux autorités compétentes toutes données requises pour la recherche et l’identification de l’auteur d’une infraction.
+			3.5 Il appartient à l’utilisateur de s’assurer de la confidentialité du ou des mots de passe utilisés sur le site www.breakem-all.com. L'administration ne dispose d’aucun mot de passe et ne saurait être tenue pour responsable en cas de perte ou d’utilisation non conforme de cet élément. L’utilisateur est invité à signaler toute utilisation non autorisée de son espace utilisateur.
 			</li>
 		</ul>
 		</article>
 		<article>
-		Article 4 : Utilisation des services
-		4.1 L’utilisateur s’engage à respecter l’ensemble des règles fixées par les présentes dans l’utilisation des services du site www.jeuxvideo.com. Il s’engage notamment, à ne pas créer, diffuser, transmettre, communiquer ou stocker, par tous moyens que ce soit et quel qu’en soit le destinataire, de contenus illicites ou propos diffamatoires, injurieux, discriminatoires, dénigrants ou contrevenants à l’ordre public.
-
-		4.2 Il s’engage également à respecter la vie privée de l’ensemble des autres utilisateurs et à ne pas porter atteinte aux droits des tiers.
-
-		4.3 L’utilisateur s’interdit de porter atteinte de quelque manière que ce soit à l’ensemble des droits de propriété intellectuelle afférents au site www.jeuxvideo.com, ainsi qu’à ses contenus et services. Il s’engage notamment à ne pas reproduire, copier, diffuser, distribuer, communiquer, céder, représenter sur tout autre site Web, ainsi que sur tout support ayant une finalité commerciale les éléments et contenus du site www.jeuxvideo.com et de ses services.
-
-		4.4 L’utilisateur s’engage à ne pas entraver, perturber, détourner et plus généralement à ne pas agir d’une quelconque façon, qui ne serait pas en conformité avec l’usage ordinaire du site www.jeuxvideo.com.
-
-		4.5 L’utilisateur s’interdit de diffuser et/ou de prospecter à des fins commerciales sur le site www.jeuxvideo.com, il s’interdit d’y introduire des contenus pouvant porter préjudice aux autres utilisateurs.
-
-		4.6 Lorsqu’il fait le choix de recourir aux services du site tels que « Commentaires » et plus généralement lorsqu’il souhaite diffuser un contenu sur le site www.jeuxvideo.com, l’utilisateur accepte, que ce contenu sera accessible pour une durée indéterminée. L’utilisateur renonce à toute revendication pécuniaire au titre de ce contenu, de sa création et de sa diffusion. Sur ce dernier point, il est précisé que par son utilisation du site, l’utilisateur s’accorde avec Webedia sur « l’objectif d’intérêt public » et la « nécessaire gratuité » qui résultent de sa contribution.
-
-		4.7 L’utilisateur reconnaît le caractère public, consultable par tous, des espaces de discussions (Forums, Chat, Témoignages etc…) et par conséquent s’interdit d’y diffuser des coordonnées ou données personnelles.
-
-		4.9 Conformément à la loi n° 2004-575 du 21 juin 2004, les utilisateurs sont invités à signaler tout élément de contenu présentant un caractère illicite : http://www.jeuxvideo.com/mailform.php.
+		<p>Article 4 : Utilisation des services</p>
+		<ul>
+			<li>
+			4.1 L’utilisateur s’engage à respecter l’ensemble des règles fixées par les présentes dans l’utilisation des services du site. Il s’engage notamment, à ne pas créer, diffuser, transmettre, communiquer ou stocker, par tous moyens que ce soit et quel qu’en soit le destinataire, de contenus illicites ou propos diffamatoires, injurieux, discriminatoires, dénigrants ou contrevenants à l’ordre public.
+			</li>
+			<li>
+			4.2 Il s’engage également à respecter la vie privée de l’ensemble des autres utilisateurs et à ne pas porter atteinte aux droits des tiers.
+			</li>
+			<li>
+			4.3 L’utilisateur s’interdit de porter atteinte de quelque manière que ce soit à l’ensemble des droits de propriété intellectuelle afférents au site www.breakem-all.com, ainsi qu’à ses contenus et services. Il s’engage notamment à ne pas reproduire, copier, diffuser, distribuer, communiquer, céder, représenter sur tout autre site Web, ainsi que sur tout support ayant une finalité commerciale les éléments et contenus du site www.breakem-all.com et de ses services.
+			</li>
+			<li>
+			4.4 L’utilisateur s’interdit de diffuser et/ou de prospecter à des fins commerciales sur le site www.jeuxvideo.com, il s’interdit d’y introduire des contenus pouvant porter préjudice aux autres utilisateurs.
+			</li>
+			<li>
+			4.5 Lorsqu’il fait le choix de recourir aux services du site tels que « Commentaires » et plus généralement lorsqu’il souhaite diffuser un contenu sur le site www.breakem-all.com, l’utilisateur accepte, que ce contenu sera accessible pour une durée indéterminée. L’utilisateur renonce à toute revendication pécuniaire au titre de ce contenu, de sa création et de sa diffusion.
+			</li>
+			<li>
+			4.6 L’utilisateur reconnaît le caractère public, consultable par tous, des espaces de discussions et par conséquent s’interdit d’y diffuser des coordonnées ou données personnelles.
+			</li>
+			<li>
+			4.7 Conformément à la loi n° 2004-575 du 21 juin 2004, les utilisateurs sont invités à signaler tout élément de contenu présentant un caractère illicite.
+			</li>
+		</ul>
 		</article>
 		<article>
-		Article 6 : Dispositions relatives à l’utilisation du service « Messagerie »
-		6.1 L’utilisation du service « Messagerie » est accessible à tout internaute sous réserve d’inscription et d’acceptation des présentes conditions générales d’utilisation. Les messages échangés sur le service « Messagerie » relèvent de la pleine et entière responsabilité de leurs auteurs. La société Webedia n’exerce pas de modération de ces messages.
+		<p>Article 5 : Responsabilité</p>
+		<ul>
+			<li>
+			5.1 
+			L’utilisateur reconnaît expressément utiliser le site et les services www.jeuxvideo.com à ses seuls et entiers risques et périls.
+
+			breakem-all.com ne pourra être tenue pour responsable des dommages (directs ou indirects), survenus lors de l’utilisation de son site ou de ses services, à l’exception des dommages qui auraient pour cause un manquement à ses obligations.
+			</li>
+			<li>
+			5.2 L’utilisateur accepte de se soumettre aux dispositions relatives à la force majeure. La force majeure est constituée par tout événement revêtant les caractères d’extériorité, d’irrésistibilité et d’imprévisibilité reconnus par la jurisprudence des cours et des tribunaux qui empêcheraient l’une des parties ou les deux d’exécuter tout ou partie des engagements contenus dans les présentes.
+			</li>
+			<li>
+			5.3 breakem-all.com le droit de modifier, supprimer ou neutraliser tout ou partie des services proposés sur le site, à tout moment et sans avoir à en justifier.
+			</li>
+		</ul>
 		</article>
 		<article>
-		Article 7 : Responsabilité
-		7.1 La société Webedia met en place les moyens pour assurer un site et des services de qualité. Néanmoins, la société Webedia ne saurait être tenue pour responsable d’une quelconque indisponibilité, défaillance, modification ou erreur survenue lors de l’utilisation de son site ou de ses services à l’exception de celles prévues dans les conditions spécifiques inhérentes à certains services. La société Webedia ne saurait garantir la continuité, l’accessibilité et la sécurité absolue du service, compte tenu notamment des risques liés à Internet.
-
-		L’utilisateur reconnaît expressément utiliser le site et les services www.jeuxvideo.com à ses seuls et entiers risques et périls.
-
-		La société Webedia ne pourra être tenue pour responsable des dommages (directs ou indirects), survenus lors de l’utilisation de son site ou de ses services, à l’exception des dommages qui auraient pour cause un manquement à ses obligations.
-
-		7.3 L’utilisateur accepte de se soumettre aux dispositions relatives à la force majeure. La force majeure est constituée par tout événement revêtant les caractères d’extériorité, d’irrésistibilité et d’imprévisibilité reconnus par la jurisprudence des cours et des tribunaux qui empêcheraient l’une des parties ou les deux d’exécuter tout ou partie des engagements contenus dans les présentes.
-
-		7.4 La société Webedia se réserve le droit de modifier, supprimer ou neutraliser tout ou partie des services proposés sur le site www.jeuxvideo.com, à tout moment et sans avoir à en justifier.
+		<p>Article 6 : Prix</p>
+		L’accès au site www.breakem-all.com est entièrement gratuit.
 		</article>
 		<article>
-		Article 8 : Prix
-		L’accès au site www.bream-all.com est entièrement gratuit.
+		<p>Article 7 : Propriété intellectuelle</p>
+		7.1 Le site www.breakem-all.com, ses contenus et services, modèles, bases de données, marques et logos sont soumis au droit de la Propriété intellectuelle. La référence à ces droits figure également dans les mentions légales. L’utilisateur s’engage à respecter ces droits.
 		</article>
 		<article>
-		Article 9 : Propriété intellectuelle
-		9.1 Le site www.jeuxvideo.com, ses contenus et services, ses logiciels, dessins, modèles, bases de données, marques et logos sont soumis au droit de la Propriété intellectuelle. La référence à ces droits figure également dans les mentions légales. Ces différents éléments sont la propriété de la société Webedia. L’utilisateur s’engage à respecter ces droits. La société Webedia ne confère à l’utilisateur qu’un droit non exclusif et incessible d’utilisation (l’utilisation s’entend d’un usage non commercial, caractérisé par la navigation, la participation et le choix de la souscription aux différents services) de son site et de ses services, et se réserve par conséquent les droits d’exploitation de diffusion, cession, ainsi que tout autre droit sur les éléments qui constituent son site et ses services.
-		</article>
-		<article>
-		Article 10 : Durée
-		10.1 Le présent contrat est conclu entre Breakem-all et l’utilisateur, dès son acceptation par ce dernier. En cas de refus, l’utilisateur s’engage à cesser d’utiliser le site et ses services. Le contrat est conclu pour toute la durée de l’utilisation du site et des services.
-
-		10.2 Le non respect d’une quelconque obligation figurant dans les présentes conditions générales entraînera la résiliation immédiate du contrat entre Breakem All et l’utilisateur, sans préjudice d’éventuels dommages et intérêts pour Breakem All.
+		<p>Article 10 : Durée</p>
+		<ul>
+			<li>
+			10.1 Le présent contrat est conclu entre Breakem-all et l’utilisateur, dès son acceptation par ce dernier. En cas de refus, l’utilisateur s’engage à cesser d’utiliser le site et ses services. Le contrat est conclu pour toute la durée de l’utilisation du site et des services.
+			</li>
+			<li>
+			10.2 Le non respect d’une quelconque obligation figurant dans les présentes conditions générales entraînera la résiliation immédiate du contrat entre Breakem All et l’utilisateur, sans préjudice d’éventuels dommages et intérêts pour Breakem All.
+			</li>
+		</ul>	
 		</article>
 		<article>
 		Article 11 : Juridictions compétentes et droit applicable
-		Les présentes conditions générales sont soumises au Droit Français. Tout litige sera porté devant les tribunaux du ressort de la Cour d’Appel de Paris.
+		<p>Les présentes conditions générales sont soumises au Droit Français. Tout litige sera porté devant les tribunaux du ressort de la Cour d’Appel de Paris.
+		</p>
 		</article>
 		<article>
-		Article 12 : Dispositions diverses
-		12.1 Les présentes conditions générales sont conclues entre Breakem All et l’utilisateur, ce dernier ne peut les céder.
-
-		12.2 Si une ou plusieurs stipulation(s) du présent contrat est (sont) tenue(s) pour non valide(s) ou déclarée(s) telle(s) en application d’une loi, d’un règlement ou à la suite d’une décision définitive d’une juridiction compétente, les autres stipulations du contrat garderont toute leur force et leur portée.
-
-
-		12.4 Les présentes Conditions générales sont les seules applicables pour le site bream-all.com ; elles s’appliquent en intégralité. L’utilisateur ne peut les modifier.
-
-		12.5 Breakem-all se réserve le droit de modifier à tout moment les présentes Conditions générales. L’utilisateur a le devoir de se tenir informé de toute actualisation. Il est à la charge de l’utilisateur de conserver les Conditions générales et les Conditions Générales Spécifiques des services pour lesquels il a contracté.
+		<p>Article 12 : Dispositions diverses</p>
+		<ul>
+			<li>
+			12.1 Les présentes conditions générales sont conclues entre Breakem All et l’utilisateur, ce dernier ne peut les céder.
+			</li>
+			<li>
+			12.2 Si une ou plusieurs stipulation(s) du présent contrat est (sont) tenue(s) pour non valide(s) ou déclarée(s) telle(s) en application d’une loi, d’un règlement ou à la suite d’une décision définitive d’une juridiction compétente, les autres stipulations du contrat garderont toute leur force et leur portée.
+			</li>
+			<li>
+			12.4 Les présentes Conditions générales sont les seules applicables pour le site breakem-all.com ; elles s’appliquent en intégralité. L’utilisateur ne peut les modifier.
+			</li>
+			<li>
+			12.5 Breakem-all se réserve le droit de modifier à tout moment les présentes Conditions générales. L’utilisateur a le devoir de se tenir informé de toute actualisation. Il est à la charge de l’utilisateur de conserver les Conditions générales et les Conditions Générales Spécifiques des services pour lesquels il a contracté.
+			</li>
+		</ul>
 		</article>
 	</section>
 </div>
