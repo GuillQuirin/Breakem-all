6c13e482aced43d2440f729babe0c24352afbdd1
Dylan <dylanfoster.esgi@gmail.com>
2016-07-09 11:49:26 +0200

    matchsManager -> correction de la requete recuperant tous les matchs d'un tournoi : le where va après la jointure

--- models/matchsManager.class.php
+++ models/matchsManager.class.php
@@ -5,12 +5,10 @@
 final class matchsManager extends basesql{
 	public function getMatchsOfTournament(tournament $t){
 		$sql = "SELECT DISTINCT(m.id), m.idWinningTeam, m.proof, m.idTournament, m.startDate, m.matchNumber 
-		FROM matchs m 
-		WHERE m.idTournament = :idTournament";
-		// On est obligé de rajouter les % sur les values des array
-		// 	les mettre dans la requete ne fonctionnant apparemment pas
+		FROM matchs m ";
 		$sql .= " LEFT OUTER JOIN matchparticipants mp ON mp.idMatch = m.id";
-
+		$sql .= " WHERE m.idTournament = :idTournament";
+		// echo $sql;
 		$sth = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 		$sth->execute([
 			':idTournament' => $t->getId()
@@ -21,9 +19,8 @@ final class matchsManager extends basesql{
 			$r[0] = array_filter($r[0]);
 			if(is_array($r[0])){
 				foreach ($r as $key => $data) {
-					if(count(array_filter($data)) > 0){
+					if(count(array_filter($data)) > 0)
 						$allMatchs[] = new matchs($data);
-					}
 				}
 			}			
 			return (count($allMatchs) > 0) ? $allMatchs : false;
