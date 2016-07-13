bcec2531bb6356f0c934a7d688cf16a7e97930d2
Rémy Cariou <Rmy Cariou>
2016-07-13 18:14:49 +0200

    Formulaire de cration de team sous forme de pop up dans liste des teams

--- controllers/teamController.class.php
+++ controllers/teamController.class.php
@@ -39,6 +39,16 @@ class teamController extends template{
         $v->assign("listeteam", $listeteam);
      
         $v->setView("team");
+
+        if(isset($_SESSION['err_name'])){
+            $v->assign("err_name","1");
+            unset($_SESSION["err_name"]);
+        }
+
+        if(isset($_SESSION['err_desc'])){
+            $v->assign("err_desc","1");
+            unset($_SESSION["err_desc"]);
+        }
     }
 
     /******************* VOIR SI FONCTIONS ENCORE PERTINENTES ***************************/
@@ -117,4 +127,65 @@ class teamController extends template{
 
         echo json_encode(["success" => true]);
 	}
+
+
+
+    public function addTeamAction()
+    {
+
+
+
+        $args = array(
+            'name' => FILTER_SANITIZE_STRING,
+            'description' => FILTER_SANITIZE_STRING,
+            'slogan' => FILTER_SANITIZE_STRING
+        );
+
+        $filteredinputs = array_filter(filter_input_array(INPUT_POST, $args));
+        if(strlen($filteredinputs['name']) < 2 || strlen($filteredinputs['name']) > 20 ||  $filteredinputs['name'] == "" ){
+            $_SESSION['err_name']=$filteredinputs['name'];
+        }else if(strlen($filteredinputs['description']) < 3 || strlen($filteredinputs['description']) > 20 ||  $filteredinputs['description'] == "" ){
+            $_SESSION['err_desc']=$filteredinputs['description'];
+            header('Location: '.WEBPATH.'/creationteam.php;');
+        }
+
+        if (isset($_FILES['img']) && $_FILES['img']['error'] != 4) {
+            $uploaddir = '/web/img/upload/';
+            $name = $_FILES['img']['name'];
+
+            $uploadfile = getcwd().$uploaddir.$name;
+
+            define('KB', 1024);
+            define('MB', 1048576);
+            define('GB', 1073741824);
+            define('TB', 1099511627776);
+
+            if ($_FILES['img']['size'] < 1 * MB) {
+                if ($_FILES['img']['error'] == 0) {
+
+                    if (!move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile))
+                        die("Erreur d'upload");
+                }
+            }
+            $filteredinputs['img'] = $name;
+        }
+
+        $filteredinputs['id_user_creator'] = $this->connectedUser->getId();
+
+        $teamBDD = new teamManager();
+        $teamBDD->mirrorObject = new team($filteredinputs);
+        $teamBDD->create();
+        $team = $teamBDD->getTeam(array('name'=>$filteredinputs['name']));
+        $t = $teamBDD->SearchIdTeam($team);
+
+        $idteam = new userManager();
+        $idteam->setNewTeamId( $this->connectedUser->getId(), $t[0]["id"] );
+
+//vérifier si la creation s'est bien passé (voir configuration de guillaume)
+//si l'user co actuellement a un id team -> redirection (template ligne 40)
+        header('Location: '.WEBPATH.'/detailteam?name='.$filteredinputs['name']);
+
+    }
+//$team = $teamBDD->getTeam(array('name'=>$filteredinputs['name']));
+
 }
--- web/js/team.js
+++ web/js/team.js
@@ -1,3 +1,34 @@
+
+$(document).ready(function () {
+	$(".formteam1").submit(function () {
+
+		if ($.trim($(".nameteam").val()) == "" || $.trim($(".nameteam").val()).length < 2 || $.trim($(".nameteam").val()).length > 10) {
+			alert("Veuillez entrer un nom de team compris entre 2 et 20 caractères.");
+			return false;
+		}
+
+		if ($.trim($(".desc-default").val()) == "" || $.trim($(".desc-default").val()).length < 3 || $.trim($(".desc-default").val()).length > 200) {
+			alert("Veuillez entrer une description comprise entre 3 et 200 caractères.");
+			return false;
+		}
+
+	});
+
+});
+
+	//Affichages des popups
+	$("#creationTeam").click(function () {
+		$(".formteam").fadeIn();
+
+	});
+
+$(document).mouseup(function(e){
+	var container = $(".formteam");
+	if(!container.is(e.target) && container.has(e.target).length === 0) {
+		container.fadeOut();
+	}
+});
+
 window.addEventListener('load', function load(){
 	// Cette ligne permet la 'supression' de l'event de load pour liberer du cache (on devrait faire ça idéalement pour tous les events utilisés une seule fois) 
 	window.removeEventListener('load', load, false);
@@ -6,49 +37,56 @@ window.addEventListener('load', function load(){
 
 
 var teamcreation = {
-	init: function(){
+
+	init: function () {
+
 		this.setFormToWatch();
-		if(this.getFormToWatch() instanceof jQuery){
+		if (this.getFormToWatch() instanceof jQuery) {
 			this.loadSendEvent();
-		};		
+		}
+		;
 	},
-	setFormToWatch: function(){this._form = jQuery("#teamcreation-form");},
-	getFormToWatch: function(){return this._form;},
-	highlightInput: function(jQinput){
+	setFormToWatch: function () {
+		this._form = jQuery("#teamcreation-form");
+	},
+	getFormToWatch: function () {
+		return this._form;
+	},
+	highlightInput: function (jQinput) {
 		jQinput.addClass('failed-input');
 		jQinput.val('');
 		jQinput.focus();
 		this.removeFailAnimationEvent(jQinput);
 	},
-	tryParseServerData: function(rawData){
+	tryParseServerData: function (rawData) {
 		try {
 			var obj = jQuery.parseJSON(rawData);
 			return obj;
 		}
-		catch(err) {
+		catch (err) {
 			console.log(rawData);
 			alert("Problem during server processes \n Check console for details");
 		}
 		return false;
 	},
-	treatParsedJson: function(obj){
+	treatParsedJson: function (obj) {
 		console.log("json successfully parsed !");
 		console.log(obj);
-		if(obj.success){
+		if (obj.success) {
 			// rediriger vers la page de la team tout juste créée
 		}
-		else{
+		else {
 			this.highlightInput(this._password);
-			if(obj.errors.nameused){
+			if (obj.errors.nameused) {
 				// Le nom est déjà pris en base
 				this.highlightInput(this._name);
 				alert("Nom de team déjà pris");
 			}
-			else if(obj.errors.userhasteam){
+			else if (obj.errors.userhasteam) {
 				// L'utilisateur a déjà une team!
 				alert("Vous êtes déjà dans une équipe");
 			}
-			else if(obj.errors.creation){
+			else if (obj.errors.creation) {
 				// La création de la team a échoué
 				alert("La création de la team a échoué, contactez un admin");
 			}
@@ -57,7 +95,7 @@ var teamcreation = {
 
 
 	/*### Send Form event ###*/
-	loadSendEvent: function(){
+	loadSendEvent: function () {
 		var _this = this;
 		var _form = this.getFormToWatch();
 		var _name = _form.find("input[name='name']");
@@ -69,39 +107,48 @@ var teamcreation = {
 		this._slogan = _slogan;
 		this._description = _description;
 
-		_submitBtn.click(function(event) {
+		_submitBtn.click(function (event) {
 			event.preventDefault();
 			jQuery.ajax({
-			  url: 'team/verify',
-			  type: 'POST',
-			  data: {
-			  	name: _name.val(),
-			  	slogan: _slogan.val(),
-			  	description: _description.val()
-			  },
-			  complete: function(xhr, textStatus) {
-			    // console.log("request complted \n");
-			  },
-			  success: function(data, textStatus, xhr) {
-			    var obj = _this.tryParseServerData(data);
-			    if(obj != false){
-			    	_this.treatParsedJson(obj);
-			    }
-			  },
-			  error: function(xhr, textStatus, errorThrown) {
-			    console.log("request error !! : \t " + errorThrown);
-			    return false;
-			  }
+				url: 'team/verify',
+				type: 'POST',
+				data: {
+					name: _name.val(),
+					slogan: _slogan.val(),
+					description: _description.val()
+				},
+				complete: function (xhr, textStatus) {
+					// console.log("request complted \n");
+				},
+				success: function (data, textStatus, xhr) {
+					var obj = _this.tryParseServerData(data);
+					if (obj != false) {
+						_this.treatParsedJson(obj);
+					}
+				},
+				error: function (xhr, textStatus, errorThrown) {
+					console.log("request error !! : \t " + errorThrown);
+					return false;
+				}
 			});
 		});
 
 	},
 
 	/*### Remove Animation on keyup event ###*/
-	removeFailAnimationEvent: function(jQInput){
+	removeFailAnimationEvent: function (jQInput) {
 		// Le one() permet de ne declencher l'event (keyup ici) qu'une seule fois puis de le supprimer automatiquement
-		jQInput.one('keyup', function() {
+		jQInput.one('keyup', function () {
 			jQInput.removeClass('failed-input');
 		});
-	}
+	},
+	clickFadeInEvent: function () {
+		//Affichages des popups
+		$("#creationTeam").click(function () {
+			$(".formteam").show();
+
+		});
+
+	},
+
 }
