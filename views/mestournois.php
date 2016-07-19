d0b6514de9412131d0aa031eb4cf00f8b7f3049b
Meksavanh <teddymeksavanh@yahoo.fr>
2016-07-19 12:45:22 +0200

    wrapper

--- views/mestournois.php
+++ views/mestournois.php
@@ -2,11 +2,23 @@
 
 	<div class="align full-height">
 		<div class="align full-height animation fadeLeft">
-			<div class="header-title admin-header-title border-full relative mestournois-title-container display-block">				
+			<div class="header-title admin-header-title border-full relative display-block">				
 					<h1 class="capitalize title header-title">Mes tournois</h1>				
 			</div>	
 		</div>
 	</div>	 	
 
 	<img class="icon icon-size-3 down-center header-scroll-down" id="classement-header-scroll-down" src="web/img/icon/icon-scrollDown.png"> 
+</section>
+
+<section class="team-content-wrapper my-content-wrapper">
+
+	<div class="container m-a content-border team-container">
+
+		<div class="row team-content-row">
+
+		</div>
+		
+	</div>
+
 </section>
