966677d6450d4dbd088780adda7e1b161b7684b6
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-07-20 22:23:23 +0200

    Centrage du titre sur /index

--- views/confirmation.php
+++ views/confirmation.php
@@ -1,3 +1,3 @@
 <div style="margin-top: 300px;" class="grid-xs-12 display-flex-column">
-	Un email de confirmation vous a été envoyé afin de valider votre inscription
+	Un email de confirmation vous a été envoyé afin de valider votre inscription.
 </div>
