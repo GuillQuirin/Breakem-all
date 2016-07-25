b1d0b5be0a4ac26691dc009d94571f6f243bc713
Reconnois <GUI.QUIRIN@hotmail.fr>
2016-07-25 23:20:01 +0200

    Correction de la synchro du RSS

--- controllers/RSSController.class.php
+++ controllers/RSSController.class.php
@@ -9,7 +9,7 @@ class RSSController extends template{
 		$v->assign("content", "Fiche de l'utilisateur");
 
 		$tournoiBDD = new tournamentManager();
-		$listtournoi = $tournoiBDD->getRecentsTournaments(5);
+		$listtournoi = $tournoiBDD->getRecentsTournaments();
 
 		//Rédaction du fichier
 
@@ -51,9 +51,9 @@ class RSSController extends template{
 		$xml .= '</rss>';
 
 		//Ecriture dans le fichier
-		$fichier = fopen(LOCALPATH."/flux.xml", 'w');
+		$fichier = fopen(LOCALPATH."/flux.xml", 'w+');
 		if(isset($fichier) && $fichier){
-			fputs($fichier, $xml);
+			fwrite($fichier, $xml);
 			fclose($fichier);
 		}
 		//$v->setView("blank","templateEmpty");
